a6d2b06cfd6d86b0762a320c95c22b97c1d2a706
James Plummer <jamesp2001@live.co.uk>
2025-07-30 15:28:49 +0200

    defer colorscheme loading for nvim

--- dotfiles/nvim/lua/set_colorscheme.lua
+++ dotfiles/nvim/lua/set_colorscheme.lua
@@ -1,10 +1,12 @@
 local light_theme = "rusticated"
 local dark_theme = "rasmus"
-local current_theme = dark_theme
+local current_theme = light_theme
 
 local bg = current_theme == dark_theme and "dark" or "light"
 
-ColorMyPencils(current_theme, bg, true)
-DisableBold()
-DisableItalic()
-DisableUndercurl()
+vim.defer_fn(function()
+	ColorMyPencils(current_theme, bg, true)
+	DisableBold()
+	DisableItalic()
+	DisableUndercurl()
+end, 100)
