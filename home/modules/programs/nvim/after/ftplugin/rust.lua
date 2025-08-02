eaa0c6a5a140933ab710df9dbbf26b4a658307f3
James Plummer <jamesp2001@live.co.uk>
2025-08-02 13:15:01 +0200

    enable rustaceanvim keymaps

--- home/modules/programs/nvim/after/ftplugin/rust.lua
+++ home/modules/programs/nvim/after/ftplugin/rust.lua
@@ -2,16 +2,15 @@ vim.bo.tabstop = 8
 vim.bo.shiftwidth = 8
 vim.bo.softtabstop = 8
 
--- was for rustaceanvim but now using ferris.nvim
-
--- local bufnr = vim.api.nvim_get_current_buf()
--- -- MAP("n", "<leader>a", function()
--- -- 	CMD.RustLsp("codeAction") -- supports rust-analyzer's grouping
--- -- or lsp.buf.codeAction() if you don't want grouping.
--- -- end, { silent = true, buffer = bufnr })
--- MAP(
--- 	"n",
--- 	"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
--- 	function() CMD.RustLsp({ "hover", "actions" }) end,
--- 	{ silent = true, buffer = bufnr }
--- )
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "ga", function()
+	vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+end, { silent = true, buffer = bufnr })
+vim.keymap.set(
+	"n",
+	"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+	function()
+		vim.cmd.RustLsp({ "hover", "actions" })
+	end,
+	{ silent = true, buffer = bufnr }
+)
