e4d4b22f971ba524d197290f5d3b8d5c2be4ee0a
James Plummer <jamesp2001@live.co.uk>
2025-08-03 02:34:42 +0200

    nightly rust and add cargo-generate

--- home/modules/packages/rust.nix
+++ home/modules/packages/rust.nix
@@ -8,14 +8,15 @@
 
 {
   home.packages = [
-    # fenix.packages.${pkgs.system}.complete.toolchain # nightly
-    fenix.packages.${pkgs.system}.stable.toolchain # stable
+    fenix.packages.${pkgs.system}.complete.toolchain # nightly
+    # fenix.packages.${pkgs.system}.stable.toolchain # stable
     pkgs.cargo-binstall
     pkgs.bacon
     bacon-ls.defaultPackage.${system}
     pkgs.cargo-careful
     pkgs.cargo-deny
     pkgs.cargo-fuzz
+    pkgs.cargo-generate
     pkgs.cargo-nextest
     pkgs.cargo-machete
     pkgs.cargo-workspaces
