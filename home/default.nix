d9a0c3fcf3b0112c01f7ce3ed2e4b875c1f4c417
James Plummer <jamesp2001@live.co.uk>
2025-08-08 16:32:57 +0200

    generate rebuild/rollback scripts

--- home/default.nix
+++ home/default.nix
@@ -28,6 +28,45 @@
       ;
   };
 
+  # generated scripts for cross-platform rebuilds
+  home.file."nixos-config/rebuild.nu" = {
+    text = ''
+      #!/usr/bin/env nu
+
+      def main [] {
+          let os = (sys host | get name)
+          
+          if $os == "Darwin" {
+              print "Building Darwin configuration..."
+              sudo darwin-rebuild switch --flake ~/nixos-config#darwin
+          } else {
+              print "Building NixOS configuration..."
+              sudo nixos-rebuild switch --flake ~/nixos-config#nixos
+          }
+      }
+    '';
+    executable = true;
+  };
+
+  home.file."nixos-config/rollback.nu" = {
+    text = ''
+      #!/usr/bin/env nu
+
+      def main [] {
+          let os = (sys host | get name)
+          
+          if $os == "Darwin" {
+              print "Rolling back Darwin configuration..."
+              sudo darwin-rebuild switch --rollback --flake ~/nixos-config#darwin
+          } else {
+              print "Rolling back NixOS configuration..."
+              sudo nixos-rebuild switch --rollback --flake ~/nixos-config#nixos
+          }
+      }
+    '';
+    executable = true;
+  };
+
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 
