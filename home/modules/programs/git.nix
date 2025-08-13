207b223d28cc9000b3734de3d3b9c8656f901f9b
James Plummer <jamesp2001@live.co.uk>
2025-08-13 13:47:37 +0200

    smarter git add handling

--- home/modules/programs/git.nix
+++ home/modules/programs/git.nix
@@ -82,7 +82,10 @@
 
     aliases = {
       diff-stat = "diff --stat --ignore-space-change -r";
+      d = "diff";
+      ds = "diff --staged";
       cm = "commit -m";
+      ca = "commit -a";
       aa = "add .";
       ap = "add -p";
       lg = "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
@@ -91,6 +94,29 @@
       sw = "switch";
       oneln = "log --oneline";
       sus = "!f() { git branch --set-upstream-to $1; }; f";
+      a = ''
+        !f() { \
+                for pattern in "$@"; do \
+                    matches=$(git ls-files | grep -i "$pattern"); \
+                    if [ -z "$matches" ]; then \
+                        echo "no files found matching \"$pattern\""; \
+                        continue; \
+                    fi; \
+                    if [ $(echo "$matches" | wc -l) -eq 1 ]; then \
+                        git add "$matches"; \
+                        echo "added: $matches"; \
+                    else \
+                        echo "multiple matches for \"$pattern\":"; \
+                        echo "$matches"; \
+                        printf "add all? (y/n): "; \
+                        read response; \
+                        if echo "$response" | grep -q "^[Yy]"; then \
+                            echo "$matches" | xargs git add; \
+                            echo "added all matches"; \
+                        fi; \
+                    fi; \
+                done; \
+            }; f'';
     };
   };
 }
