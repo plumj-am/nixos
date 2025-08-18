6ac1d2d151976de25165e41e746f7c85f4f8e873
James Plummer <jamesp2001@live.co.uk>
2025-08-19 00:15:54 +0200

    change dark nvim theme

--- home/modules/editor/nvim/set_colorscheme.lua
+++ home/modules/editor/nvim/set_colorscheme.lua
@@ -1,5 +1,5 @@
 local light_theme = "zenwritten"
-local dark_theme = "rasmus"
+local dark_theme = "zenbones"
 local current_theme = light_theme
 
 local bg = current_theme == dark_theme and "dark" or "light"
