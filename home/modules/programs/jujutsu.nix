4aec30c6bed435de68046e0a23084222797e3874
James Plummer <jamesp2001@live.co.uk>
2025-08-27 20:26:54 +0200

    jj: improved config and more aliases

--- home/modules/programs/jujutsu.nix
+++ home/modules/programs/jujutsu.nix
@@ -1,70 +1,101 @@
-{ pkgs, ... }:
-{
-  home.packages = [
-    pkgs.jjui
-  ];
-  programs.git.difftastic.enable = true;
-  programs.mergiraf.enable = true;
-  programs.jujutsu = {
-    enable = true;
+{ pkgs, lib, ... }: 
+let
+  inherit (lib) enabled;
+in {
+  home.packages = [ 
+		pkgs.jjui 
+	];
+
+  programs.git.difftastic = enabled;
+
+  programs.mergiraf = enabled;
+
+	# credit to https://github.com/rgbcube/ncc for most of this
+  programs.jujutsu = enabled {
     settings = {
-      user = {
-        name = "James Plummer";
-        email = "jamesp2001@live.co.uk";
-      };
-      signing = {
-        key = "58805BF7676222B4";
-        backend = "gpg";
-        behavior = "own";
-      };
-      ui = {
-        editor = "nvim";
-        default-command = "log --summary";
-        conflict-marker-style = "snapshot";
-        diff-editor = ":builtin";
-        graph-style = "square";
-        diff-formatter = [
-          "difft"
-          "--color"
-          "always"
-          "$left"
-          "$right"
-        ];
-      };
-      git = {
-        sign-on-push = true; # sign in bulk on push
-        auto-local-bookmark = true;
-      };
-      aliases = {
-        push = [
-          "git"
-          "push"
-        ];
-        P = [
-          "git"
-          "push"
-        ];
-        fetch = [
-          "git"
-          "fetch"
-        ];
-        f = [
-          "git"
-          "fetch"
-        ];
-        init = [
-          "git"
-          "init"
-          "--colocate"
-        ];
-        clone = [
-          "git"
-          "clone"
-          "--colocate"
-        ];
-        d = [ "diff" ];
-        ds = [ "diff --staged" ];
-      };
+      user.name  = "James Plummer";
+      user.email = "jamesp2001@live.co.uk";
+
+      signing.key      = "58805BF7676222B4";
+      signing.backend  = "gpg";
+      signing.behavior = "own";
+
+			ui.conflict-marker-style = "snapshot";
+			ui.default-command       = "ls";
+			ui.diff-editor           = ":builtin";
+			ui.diff-formatter        = [ "difft" "--color" "always" "$left" "$right" ];
+      ui.editor                = "nvim";
+      ui.graph-style           = "square";
+
+      git.sign-on-push        = true; # sign in bulk on push
+      git.auto-local-bookmark = true;
+
+			aliases.".." = [ "edit" "@-" ];
+			aliases.",," = [ "edit" "@+" ];
+
+			aliases.a = [ "abandon" ];
+
+			aliases.c  = [ "commit" ];
+			aliases.ci = [ "commit" "--interactive" ];
+
+			aliases.e = [ "edit" ];
+
+			aliases.fetch = [ "git" "fetch" ];
+			aliases.f     = [ "git" "fetch" ];
+
+			aliases.r = [ "rebase" ];
+
+			aliases.res         = [ "resolve" ];
+			aliases.resolve-ast = [ "resolve" "--tool" "mergiraf" ];
+			aliases.resa        = [ "resolve-ast" ];
+
+			aliases.s  = [ "squash" ];
+			aliases.si = [ "squash" "--interactive" ];
+
+			aliases.sh = [ "show" ];
+
+			aliases.tug = [ "bookmark" "move" "--from" "closest(@-)" "--to" "closest_pushable(@)" ];
+			aliases.t   = [ "tug" ];
+
+      aliases.push = [ "git" "push" ];
+      aliases.P    = [ "git" "push" ];
+
+      aliases.init  = [ "git" "init" "--colocate" ];
+			aliases.i     = [ "git" "init" "--colocate" ];
+
+      aliases.clone = [ "git" "clone" "--colocate" ];
+			aliases.cl    = [ "git" "clone" "--colocate" ];
+
+      aliases.d  = [ "diff" ];
+
+      aliases.l   = [ "log" ];
+      aliases.la  = [ "log" "--revisions" "::" ];
+      aliases.ls  = [ "log" "--summary" ];
+      aliases.lsa = [ "log" "--summary" "--revisions" "::" ];
+      aliases.lp  = [ "log" "--patch" ];
+			aliases.lpa = [ "log" "--patch" "--revisions" "::" ];
+
+			revset-aliases."closest(to)"          = "heads(::to & bookmarks())";
+      revset-aliases."closest_pushable(to)" = "heads(::to & ~description(exact:\"\") & (~empty() | merges()))";
+
+      revsets.log = "present(@) | present(trunk()) | ancestors(remote_bookmarks().. | @.., 8)";     
+
+			templates.draft_commit_description = /* python */ ''
+          concat(
+            coalesce(description, "\n"),
+            surround(
+              "\nJJ: This commit contains the following changes:\n", "",
+              indent("JJ:     ", diff.stat(72)),
+            ),
+            "\nJJ: ignore-rest\n",
+            diff.git(),
+          )
+        '';
+
+      templates.git_push_bookmark = /* python */ ''
+        "change-jamesukiyo-" ++ change_id.short()
+      '';
+
     };
   };
 }
