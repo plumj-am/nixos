1d0f42db362164b130d26582954c74db5baed0d3
James Plummer <jamesp2001@live.co.uk>
2025-08-28 16:26:28 +0200

    nushell: format file

--- home/modules/shell/nushell.nix
+++ home/modules/shell/nushell.nix
@@ -6,29 +6,38 @@ in
   programs.nushell = enabled {
     shellAliases = {
       cat = "bat";
-      ls = "eza";
-      sl = "eza";
-      ll = "eza -la";
-      la = "eza -a";
+
+      ls  = "eza";
+      sl  = "eza";
+      ll  = "eza -la";
+      la  = "eza -a";
       lsa = "eza -a";
       lsl = "eza -l -a";
-      v = "vim";
-      vi = "vim";
-      nv = "nvim";
+
+      v   = "vim";
+      vi  = "vim";
+      nv  = "nvim";
       nvi = "nvim";
+
       claude = "claude --continue";
-      m = "moon";
+
+      m  = "moon";
       mp = "mprocs";
       ko = "kondo";
-      g = "git";
-      gi = "git";
+
+      g   = "git";
+      gi  = "git";
       gti = "git";
-      gt = "git";
-      rebuild = "~/nixos-config/rebuild.nu";
+      gt  = "git";
+			j   = "jj";
+			
+      rebuild  = "~/nixos-config/rebuild.nu";
       rollback = "~/nixos-config/rollback.nu";
       # for some reason this doesn't work and is called every time I open a new shell
-      # nixos-upgrade = "sudo nix-store --verify --check-contents --repair ; sudo nix flake update --flake ~/nixos-config ; sudo nixos-rebuild switch --flake ~/nixos-config#nixos-wsl";
+      # upgrade  = "sudo nix-store --verify --check-contents --repair ; sudo nix flake update --flake ~/nixos-config ; sudo nixos-rebuild switch --flake ~/nixos-config#nixos-wsl";
+
       tt = "toggle-theme";
+
       cdr = "cd (git rev-parse --show-toplevel | str trim)";
       cdn = "cd ~/nixos-config/dotfiles/nvim";
       cdc = "cd ~/nixos-config";
@@ -43,127 +52,98 @@ in
       tree = "eza --tree --git-ignore --group-directories-first";
 
       # steam-run for dynamically linked executables
-      moon = "steam-run moon";
+      moon  = "steam-run moon";
       proto = "steam-run proto";
-      bun = "steam-run bun";
-      bunx = "steam-run bunx";
-      npx = "steam-run npx";
+      bun   = "steam-run bun";
+      bunx  = "steam-run bunx";
+      npx   = "steam-run npx";
     };
     settings = {
-      edit_mode = "vi";
+      edit_mode     = "vi";
       buffer_editor = "nvim";
-      show_banner = false;
-      footer_mode = "auto";
+      show_banner   = false;
+      footer_mode   = "auto";
 
       recursion_limit = 100;
-      error_style = "fancy";
+      error_style     = "fancy";
 
-      completions = {
-        algorithm = "substring";
-        sort = "smart";
-        case_sensitive = false;
-        quick = true;
-        partial = true;
-        use_ls_colors = true;
-      };
+      completions.algorithm      = "substring";
+			completions.sort           = "smart";
+			completions.case_sensitive = false;
+			completions.quick          = true;
+			completions.partial        = true;
+			completions.use_ls_colors  = true;
 
-      ls = {
-        use_ls_colors = true;
-        clickable_links = true;
-      };
+      ls.use_ls_colors   = true;
+      ls.clickable_links = true;
 
-      rm = {
-        always_trash = false;
-      };
+      rm.always_trash = false;
 
       keybindings = [
         {
-          name = "quit_shell";
+          name     = "quit_shell";
           modifier = "control";
-          keycode = "char_d";
-          mode = [
-            "emacs"
-            "vi_insert"
-            "vi_normal"
-          ];
-          event = null;
+          keycode  = "char_d";
+          mode     = [ "emacs" "vi_insert" "vi_normal" ];
+          event    = null;
         }
       ];
-      table = {
-        mode = "compact";
-        index_mode = "always";
-        show_empty = true;
-        trim = {
-          methodology = "wrapping";
-          wrapping_try_keep_words = true;
-          truncating_suffix = "...";
-        };
-      };
 
-      explore = {
-        help_banner = true;
-        exit_esc = true;
-        command_bar_text = "#C4C9C6";
-        status_bar_background = { };
-        highlight = {
-          bg = "yellow";
-          fg = "black";
-        };
-        status = { };
-        try = { };
-        table = {
-          split_line = "#404040";
-          cursor = true;
-          line_index = true;
-          line_shift = true;
-          line_head_top = true;
-          line_head_bottom = true;
-          show_head = true;
-          show_index = true;
-        };
-        config = {
-          cursor_color = {
-            bg = "yellow";
-            fg = "black";
-          };
-        };
-      };
+			table.mode       = "compact";
+			table.index_mode = "always";
+			table.show_empty = true;
 
-      history = {
-        file_format = "sqlite";
-        max_size = 10000000;
-        sync_on_enter = true;
-      };
+			table.trim.methodology             = "wrapping";
+			table.trim.wrapping_try_keep_words = true;
+			table.trim.truncating_suffix       = "...";
 
-      filesize = { };
+			explore.help_banner           = true;
+			explore.exit_esc              = true;
+			explore.command_bar_text      = "#C4C9C6";
+			explore.status_bar_background = {};
+			explore.highlight.bg          = "yellow"; 
+			explore.highlight.fg          = "black";
+			explore.status                = {};
+			explore.try                   = {};
 
-      cursor_shape = {
-        emacs = "block";
-        vi_insert = "block";
-        vi_normal = "block";
-      };
+			explore.table.split_line       = "#404040";
+			explore.table.cursor           = true;
+			explore.table.line_index       = true;
+			explore.table.line_shift       = true;
+			explore.table.line_head_top    = true;
+			explore.table.line_head_bottom = true;
+			explore.table.show_head        = true;
+			explore.table.show_index       = true;
 
-      float_precision = 2;
+			explore.config.cursor_color.bg = "yellow";
+			explore.config.cursor_color.fg = "black";
+
+      history.file_format   = "sqlite";
+			history.max_size      = 10000000;
+			history.sync_on_enter = true;
+
+      filesize = {};
+
+      cursor_shape.emacs     = "block";
+			cursor_shape.vi_insert = "block";
+			cursor_shape.vi_normal = "block";
+
+      float_precision   = 2;
       use_ansi_coloring = "auto";
 
-      hooks = {
-        env_change = {
-          PWD = [
-            ''{ |before, after| zellij-update-tabname }''
-          ];
-        };
-        display_output = "if (term size).columns >= 100 { table -e } else { table }";
-        pre_prompt = [
-          ''
-            if (which direnv | is-empty) {
-              return
-            }
-            direnv export json | from json | default {} | load-env
-            $env.PATH = ($env.PATH | split row (char env_sep))
-          ''
-        ];
-      };
+      hooks.env_change.PWD = [ ''{ |before, after| zellij-update-tabname }'' ];
 
+      hooks.display_output = "if (term size).columns >= 100 { table -e } else { table }";
+
+			hooks.pre_prompt     = [
+				''
+					if (which direnv | is-empty) {
+						return
+					}
+					direnv export json | from json | default {} | load-env
+					$env.PATH = ($env.PATH | split row (char env_sep))
+				''
+			];
     };
 
     extraConfig = ''
