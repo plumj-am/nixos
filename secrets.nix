19babccf07fccd82852edb17b25f7e29a9eaf276
James Plummer <jamesp2001@live.co.uk>
2025-08-30 19:27:31 +0200

    hosts.plum: fix ssh key and add id

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,6 @@
 let
 	james = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA7WV4+7uhIWQVHEN/2K0jJPTaZ/HbG3W8OKSpzmPBI4";
-	plum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7VV+Jow/4g+m8CY1JRsuAngSrazPzmzchbnpTyEHA8 root@plum";
+	plum  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBH1S3dhOYCCltqrseHc3YZFHc9XU90PsvDo7frzUGrr root@plum";
 	# users = [ james ];
 in
 {
