3c01d4ef10c207e614e9165eb99aefb3e3a02a78
James Plummer <jamesp2001@live.co.uk>
2025-08-31 18:28:24 +0200

    hosts.kiwi: fix pub key and rekey

--- secrets.nix
+++ secrets.nix
@@ -2,7 +2,7 @@ let
 	james = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA7WV4+7uhIWQVHEN/2K0jJPTaZ/HbG3W8OKSpzmPBI4";
 	plum  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBH1S3dhOYCCltqrseHc3YZFHc9XU90PsvDo7frzUGrr root@plum";
 	pear  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL2/Pg/5ohT3Dacnzjw9pvkeoQ1hEFwG5l1vRkr3v2sQ root@pear";
-	kiwi  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK6ajPZhxyFlQonoT5QvbvztN1511lNq5MULcTziDT91 root@kiwi";
+	kiwi  = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1AbTRPJiOAPE0u1HHqoMBeXhlenugGvIndnJVLETld root@kiwi";
 	# users = [ james ];
 in
 {
