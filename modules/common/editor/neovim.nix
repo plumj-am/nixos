669ab607c3dd21d434e1916a72151fa336aba160
James Plummer <git@plumj.am>
2025-09-09 20:11:17 +0200

    nvim: move options to root config file

--- modules/common/editor/neovim.nix
+++ modules/common/editor/neovim.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ pkgs, lib, config, ... }:
 let
 	inherit (lib) enabled;
 in
@@ -8,9 +8,80 @@ in
   programs.vim = enabled;
 
   programs.nvf = enabled {
-    settings = {
-      vim.viAlias = true;
-      vim.vimAlias = true;
+
+    settings.vim = {
+      enableLuaLoader = true;
+      additionalRuntimePaths = [ ./after ];
+      extraLuaFiles = [
+        (builtins.path { path = ./nvim/set_colorscheme.lua; })
+      ];
+
+      clipboard = enabled {
+        registers = "unnamedplus";
+        providers.wl-copy.enable = lib.mkIf pkgs.stdenv.isLinux true;
+      };
+
+      globals = {
+        mapleader = " ";
+
+        editorconfig = true;
+
+        loaded_netrw       = 1;
+        loaded_netrwPlugin = 1;
+
+        # theme configuration for lua
+        theme_name    = config.theme.nvim;
+        theme_variant = config.theme.variant;
+        is_dark       = config.theme.is_dark;
+      };
+
+      options = {
+        # line numbers
+        number         = true;
+        relativenumber = true;
+
+        # search
+        ignorecase = true;
+        smartcase  = true;
+
+        # indentation
+        tabstop     = 4;
+        shiftwidth  = 4;
+        softtabstop = 4;
+        smartindent = true;
+
+        # split handling
+        splitbelow = true;
+        splitright = true;
+
+        # ui
+        wrap          = false;
+        pumheight     = 8;
+        termguicolors = true;
+        cursorline    = true;
+        showmode      = false;
+        showtabline   = 0;
+        guicursor     = "";
+        fillchars     = "eob:~,fold: ,foldopen:,foldsep: ,foldclose:,vert:┃,lastline:┃";
+        winborder     = "single";
+
+        # backups etc.
+        undofile    = true;
+        swapfile    = false;
+        backup      = false;
+        writebackup = false;
+
+        # scrolling
+        scrolloff     = 8;
+        sidescrolloff = 10;
+        smoothscroll  = true;
+
+        # misc
+        updatetime  = 50;
+        mouse       = "";
+        completeopt = "menuone,noselect";
+        foldenable  = false;
+      };
     };
   };
 }
