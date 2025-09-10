70391d7101752306cbf2f8d0203e4c11a698a06b
James Plummer <git@plumj.am>
2025-09-10 21:48:47 +0200

    nix: update substituters and increase max http connections

--- flake.nix
+++ flake.nix
@@ -4,18 +4,16 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.plumj.am/"
-      "https://nix-community.cachix.org/"
-      "https://cache.nixos.org/"
       "https://cache.garnix.io/"
       "https://numtide.cachix.org/"
+      "https://nix-community.cachix.org/"
     ];
 
     extra-trusted-public-keys = [
       "cache.plumj.am:NnfLDMdPEH3LE5WqFAilb4/UoIUfb0lZoFv2MoeMBbA="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy2Yk6H6WdHjLQ0Rb5h9R0OXiEiL8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
     experimental-features = [
@@ -26,7 +24,7 @@
 
     builders-use-substitutes = true;
     flake-registry           = "";
-    http-connections         = 50;
+    http-connections         = 0;
     lazy-trees               = true;
     max-jobs                 = "auto";
     show-trace               = true;
