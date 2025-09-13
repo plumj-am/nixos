09f13eca204c18ecfb8e27b986abbb1ef8da50d8
James Plummer <git@plumj.am>
2025-09-13 02:43:18 +0200

    helix: fix yazi picker

--- modules/common/editor/helix.nix
+++ modules/common/editor/helix.nix
@@ -43,7 +43,7 @@ in
 
     settings.keys = genAttrs [ "normal" "select" ] <| const {
       D = "extend_to_line_end";
-      "C-y" = ":sh zellij run -n Yazi -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh open";
+      "C-y" = ":sh zellij run -n Yazi -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- bash ~/.config/helix/yazi-picker.sh open %{buffer_name}";
     };
 
     languages.language = [
