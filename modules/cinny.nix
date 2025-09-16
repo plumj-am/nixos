7c557f322d17c56dea2cf51ea3b368df939e3a3a
James Plummer <git@plumj.am>
2025-09-16 19:30:54 +0200

    cinny: add headers to allow blob data for media

--- modules/cinny.nix
+++ modules/cinny.nix
@@ -38,6 +38,16 @@ in {
       return 200 '${toJSON cinnyConfig}';
     '';
 
+    locations."/".extraConfig = /* nginx */ ''
+      proxy_hide_header Content-Security-Policy;
+      add_header Content-Security-Policy "script-src 'self' 'unsafe-inline' 'unsafe-eval' ${domain} *.${domain}; object-src 'self' ${domain} *.${domain}; img-src 'self' data: https: blob:; base-uri 'self'; frame-ancestors 'self';" always;
+      add_header X-Frame-Options DENY always;
+      add_header X-Content-Type-Options nosniff always;
+      add_header X-XSS-Protection "1; mode=block" always;
+      add_header Permissions-Policy "camera=(), geolocation=(), payment=(), usb=()" always;
+      add_header Referrer-Policy no-referrer always;
+    '';
+
     extraConfig = /* nginx */ ''
       rewrite ^/config.json$ /config.json break;
       rewrite ^/manifest.json$ /manifest.json break;
