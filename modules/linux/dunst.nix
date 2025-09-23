c6646192ee2350ff08fe6f0545871eae74d074ad
PlumJam <git@plumj.am>
2025-09-23 11:41:58 +0200

    dunst: Add basic config for dmenu and font.

--- modules/linux/dunst.nix
+++ modules/linux/dunst.nix
@@ -2,6 +2,12 @@
   inherit (lib) mkIf enabled;
 in mkIf config.isDesktop {
   home-manager.sharedModules = [{
-    services.dunst = enabled;
+    services.dunst = with config.theme; enabled {
+      settings.global = {
+        dmenu = "fuzzel --dmenu";
+
+        font = "${font.mono.name}:size=${toString font.size.normal}";
+      };
+    };
   }];
 }
