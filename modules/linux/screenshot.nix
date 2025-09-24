339ade155196f3dd53fba1283ab98f20ff55b19e
PlumJam <git@plumj.am>
2025-09-24 15:16:15 +0200

    linux: More screenshot options with Fuzzel entries.

--- modules/linux/screenshot.nix
+++ modules/linux/screenshot.nix
@@ -2,11 +2,15 @@
   inherit (lib) mkIf;
 
   screenshot = pkgs.writeShellScriptBin "screenshot" ''
-    ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp -w 0)" - | ${pkgs.swappy}/bin/swappy -f - -o - | ${pkgs.wl-clipboard}/bin/wl-copy --type image/png
+    ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" - | ${pkgs.swappy}/bin/swappy -f -
+  '';
+
+  screenshot-clip = pkgs.writeShellScriptBin "screenshot-clip" ''
+    ${pkgs.grim}/bin/grim -g "$(${pkgs.slurp}/bin/slurp)" - | ${pkgs.wl-clipboard}/bin/wl-copy --type image/png
   '';
 
   screenshot-full = pkgs.writeShellScriptBin "screenshot-full" ''
-    ${pkgs.grim}/bin/grim - | ${pkgs.swappy}/bin/swappy -f - -o - | ${pkgs.wl-clipboard}/bin/wl-copy --type image/png
+    ${pkgs.grim}/bin/grim - | ${pkgs.wl-clipboard}/bin/wl-copy --type image/png
   '';
 
 in mkIf config.isDesktop {
@@ -15,6 +19,28 @@ in mkIf config.isDesktop {
     pkgs.slurp   # Screen area selection.
     pkgs.swappy  # Screenshot editor/annotator.
     screenshot
+    screenshot-clip
     screenshot-full
   ];
+
+  home-manager.sharedModules = [{
+    # Desktop entries allow them to appear in Fuzzel.
+    xdg.desktopEntries.screenshot = {
+      name     = "Screenshot";
+      exec     = "screenshot";
+      terminal = false;
+    };
+
+    xdg.desktopEntries.screenshot-clip = {
+      name     = "Screenshot Clipboard";
+      exec     = "screenshot-clip";
+      terminal = false;
+    };
+
+    xdg.desktopEntries.screenshot-full = {
+      name     = "Screenshot Full";
+      exec     = "screenshot-full";
+      terminal = false;
+    };
+  }];
 }
