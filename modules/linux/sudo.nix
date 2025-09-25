75993511f7042494c63869baa8b755753aadfdde
PlumJam <git@plumj.am>
2025-09-25 16:49:46 +0200

    linux.sudo: Better config and allow no password on desktop.

--- modules/linux/sudo.nix
+++ modules/linux/sudo.nix
@@ -1,7 +1,18 @@
-{ lib, ... }: let
-  inherit (lib) enabled;
+{ lib, config, ... }: let
+  inherit (lib) enabled mkIf optionalString;
 in {
   security.sudo = enabled {
     execWheelOnly = true;
+    extraConfig   = /* sudo */ ''
+      Defaults pwfeedback
+      Defaults lecture = never
+      Defaults env_keep += "DISPLAY EDITOR PATH"
+      ${optionalString config.isServer /* sudo */ ''
+        Defaults timestamp_timeout = 0
+      ''}
+    '';
+
+    wheelNeedsPassword = mkIf config.isDesktop false;
   };
+
 }
