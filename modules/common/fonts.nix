4806e5c92836c2dc5eb5ba9715bc64b1ab9ecc70
PlumJam <git@plumj.am>
2025-09-27 15:27:12 +0200

    fonts: Better config for virtual console and servers.

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -1,7 +1,16 @@
 { config, pkgs, lib, ... }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf disabled;
 in
 {
+  # Virtual console for login.
+  console = {
+    earlySetup = true;
+    font       = "Lat2-Terminus16";
+    packages   = [ pkgs.terminus_font ];
+  };
+
+  fonts.fontconfig = mkIf config.isServer disabled;
+
   fonts.packages = mkIf config.isDesktop [
     config.theme.font.mono.package
     config.theme.font.sans.package
