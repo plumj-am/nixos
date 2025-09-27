25ffab2bc691293220281fea21c0b2f80c7593c0
PlumJam <git@plumj.am>
2025-09-27 14:56:45 +0200

    kitty: Improve theme, config and add fuzzel entry.
    Using Kitty over Alacritty for the time being.

--- modules/common/kitty.nix
+++ modules/common/kitty.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: let
+{ pkgs, config, lib, ... }: let
   inherit (lib) enabled mkIf;
 in{
   home-manager.sharedModules = mkIf config.isDesktopNotWsl [{
@@ -11,20 +11,67 @@ in{
 
       settings = {
         allow_remote_control    = true;
-        conform_os_window_close = 0;
+        confirm_os_window_close = 0;
 
-        focus_follows_mouse = false;
-        mouse_hide_wait     = 0;
+        scrollback_lines = 100000;
+        scrollback_pager = "bat --chop-long-lines";
+
+        mouse_hide_wait = 0;
 
         enable_audio_bell = false;
 
-        window_padding_width = config.theme.padding;
-        window_border_width = "0pt";
+        strip_trailing_spaces = "always";
 
-        cursor     = base05;
-        background = base00;
-        foreground = base05;
+        # Performance
+        repaint_delay   = 1;
+        input_delay     = 1;
+        sync_to_monitor = false;
+
+        window_padding_width = config.theme.padding / 2;
+        window_border_width  = "0pt";
+
+        cursor                  = base05;
+        cursor_text_color       = base00;
+        background              = base00;
+        foreground              = base05;
+        active_tab_background   = base00;
+        active_tab_foreground   = base05;
+        inactive_tab_background = base01;
+        inactive_tab_foreground = base05;
+        selection_background    = base02;
+        selection_foreground    = base00;
+        url_color               = base0D;
+
+        color0  = base00;
+        color1  = base08;
+        color2  = base0B;
+        color3  = base0A;
+        color4  = base0D;
+        color5  = base0E;
+        color6  = base0C;
+        color7  = base05;
+        color8  = base03;
+        color9  = base08;
+        color10 = base0B;
+        color11 = base0A;
+        color12 = base0D;
+        color13 = base0E;
+        color14 = base0C;
+        color15 = base07;
+        color16 = base09;
+        color17 = base0F;
+        color18 = base01;
+        color19 = base02;
+        color20 = base04;
+        color21 = base06;
       };
+    };
+    # Desktop entry for Zellij in Fuzzel.
+    xdg.desktopEntries.zellij-kitty = {
+      name     = "Zellij Kitty";
+      icon     = "kitty";
+      exec     = "kitty ${pkgs.zellij}/bin/zellij";
+      terminal = false;
     };
   }];
 }
