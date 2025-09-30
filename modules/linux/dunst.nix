b61fe9b101d380b416b49c0bd9a13514d387b5e3
PlumJam <git@plumj.am>
2025-09-30 16:16:10 +0200

    dunst: Apply theme and improve config.

--- modules/linux/dunst.nix
+++ modules/linux/dunst.nix
@@ -2,12 +2,32 @@
   inherit (lib) mkIf enabled;
 in mkIf config.isDesktopNotWsl {
   home-manager.sharedModules = [{
-    services.dunst = with config.theme; enabled {
+    services.dunst = with config.theme.withHash; enabled {
       settings.global = {
-        dmenu = "fuzzel --dmenu";
+        dmenu              = "fuzzel --dmenu";
+        show_age_threshold = 0;
+
         monitor = if config.networking.hostName == "yuzu" then 1 else 0;
 
-        font = "${font.mono.name}:size=${toString font.size.normal}";
+        font = "${config.theme.font.mono.name}:size=${toString config.theme.font.size.normal}";
+
+        urgency_low = ''
+          background = "${base0E}"
+          foreground = "${base00}"
+          timeout    = 10
+        '';
+
+        urgency_normal = ''
+          background = "${base0D}"
+          foreground = "${base00}"
+          timeout    = 10
+        '';
+
+        urgency_critical = ''
+          background = "${base0B}"
+          foreground = "${base00}"
+          timeout    = 30
+        '';
       };
     };
   }];
