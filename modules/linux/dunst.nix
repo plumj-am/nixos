6ad4cb633e080d64fbfceaf5500ef3f1a9a187cc
PlumJam <git@plumj.am>
2025-09-30 21:46:09 +0200

    dunst: Improve config and theming.

--- modules/linux/dunst.nix
+++ modules/linux/dunst.nix
@@ -6,6 +6,12 @@ in mkIf config.isDesktopNotWsl {
       settings.global = {
         dmenu              = "fuzzel --dmenu";
         show_age_threshold = 0;
+        transparency       = 0;
+        separator_height   = config.theme.margin;
+        padding            = config.theme.padding;
+        horizontal_padding = config.theme.padding;
+        frame_width        = 1;
+        frame_color        = "${base00}";
 
         monitor = if config.networking.hostName == "yuzu" then 1 else 0;
 
@@ -13,19 +19,19 @@ in mkIf config.isDesktopNotWsl {
 
         urgency_low = ''
           background = "${base0E}"
-          foreground = "${base00}"
+          foreground = "${base07}"
           timeout    = 10
         '';
 
         urgency_normal = ''
           background = "${base0D}"
-          foreground = "${base00}"
-          timeout    = 10
+          foreground = "${base07}"
+          timeout    = 20
         '';
 
         urgency_critical = ''
           background = "${base0B}"
-          foreground = "${base00}"
+          foreground = "${base07}"
           timeout    = 30
         '';
       };
