ffc11d3934914cb0c339b0a46491d0eab216bf51
PlumJam <git@plumj.am>
2025-10-01 12:45:32 +0200

    pkgs: Add `fastfetch` module and a few more packages.

--- modules/common/packages.nix
+++ modules/common/packages.nix
@@ -1,27 +1,31 @@
 { config, pkgs, lib, ... }:
 {
   environment.systemPackages = [
-    pkgs.comma
-    pkgs.tree
-    pkgs.hyperfine
-    pkgs.curl
-    pkgs.tokei
 		pkgs.ast-grep
+    pkgs.comma
+    pkgs.curl
+    pkgs.hyperfine
+    pkgs.moreutils
+		pkgs.openssl
+		pkgs.rsync
+    pkgs.tokei
+    pkgs.tree
+    pkgs.typos
+    pkgs.uutils-coreutils-noprefix
   ]
   ++ lib.optionals config.isDesktop [
-    pkgs.nodejs
     pkgs.deno
-    pkgs.pnpm
-    pkgs.python3
-
     pkgs.gemini-cli
-
     pkgs.moon
+    pkgs.nodejs
+    pkgs.pnpm
     pkgs.proto
+    pkgs.python3
+    pkgs.sqlite
   ]
   ++ lib.optionals config.isLinux [
-    pkgs.wget
     pkgs.gcc
     pkgs.gnumake
+    pkgs.wget
   ];
 }
