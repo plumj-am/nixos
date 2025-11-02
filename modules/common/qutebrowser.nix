3e1d4b7fe2f14cb771fea314bf455eddc4b4f58e
PlumJam <git@plumj.am>
2025-11-02 23:55:32 +0100

    qutebrowser: Add some base16 theming.

--- modules/common/qutebrowser.nix
+++ modules/common/qutebrowser.nix
@@ -1,5 +1,5 @@
 { lib, config, ... }: let
-  inherit (lib) enabled mkIf genAttrs const;
+  inherit (lib) enabled mkIf genAttrs const merge;
 in {
 
   home-manager.sharedModules = mkIf config.isDesktopNotWsl [{
@@ -39,10 +39,40 @@ in {
 
         fonts.default_family = config.theme.font.mono.family;
         fonts.web.family     = genAttrs
-          ["sans_serif" "serif" "standard"]
+          [ "sans_serif" "serif" "standard" ]
           (const (config.theme.font.sans.name));
 
-        colors.webpage.darkmode.enabled = config.theme.is_dark;
+        colors = with config.theme.withHash; merge {
+          webpage.darkmode.enabled = config.theme.is_dark;
+
+        # Background.
+        } <| (genAttrs [ "statusbar.command.bg" "statusbar.normal.bg" "tabs.odd.bg" "tabs.selected.even.fg" "tabs.selected.odd.fg" "tabs.pinned.selected.even.fg" "tabs.pinned.selected.odd.fg" "tabs.pinned.odd.fg" "tabs.pinned.even.fg" ]
+          (const base00)
+
+        # Background 1.
+        ) // (genAttrs [ "tabs.even.bg" ]
+          (const base01)
+
+        # Background 2.
+        ) // (genAttrs [ "tabs.bar.bg" ]
+          (const base02)
+
+        # Foreground.
+        ) // (genAttrs [ "statusbar.command.fg" "statusbar.normal.fg" "statusbar.url.fg" "tabs.odd.fg" "tabs.even.fg" "tabs.selected.even.bg" "tabs.selected.odd.bg" "tabs.pinned.selected.even.bg" "tabs.pinned.selected.odd.bg" "statusbar.url.success.http.fg" "statusbar.url.success.https.fg" ]
+          (const base07)
+
+        # Red.
+        ) // (genAttrs [ "statusbar.url.error.fg" "statusbar.url.warn.fg" "tabs.indicator.error" ]
+          (const base08)
+
+        # Green.
+        ) // (genAttrs []
+          (const base0B)
+
+        # Blue.
+        ) // (genAttrs [ "statusbar.url.hover.fg"  "tabs.pinned.even.bg" "tabs.pinned.odd.bg" ]
+          (const base0D)
+        );
 
         session.lazy_restore        = true;
         auto_save.session           = true;
@@ -54,11 +84,14 @@ in {
           "keypress"
           "search_match"
           "url"
+          "text:\:\:"
           "scroll"
+          "text:\:\:"
           "history"
           "tabs"
-          # "text:qute"
+          "text:\:\:"
           "clock"
+          "text: "
         ];
       };
 
