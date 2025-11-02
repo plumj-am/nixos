3a07924c6ef7b51f1b85d334d8f882f6e048d4a0
PlumJam <git@plumj.am>
2025-11-02 23:55:32 +0100

    xdg-portal: Keep only necessary screensharing fixes.

--- modules/linux/xdg-portal.nix
+++ modules/linux/xdg-portal.nix
@@ -4,19 +4,14 @@ in mkIf config.isDesktopNotWsl {
   xdg.portal = enabled {
     config = {
       common.default = "*";
-      # Niri screensharing fixes.
-      niri = {
-        default = "*";
-
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
-      };
+      # [1/2] Niri screensharing fixes.
+      niri."org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
     };
 
     extraPortals   = [
-      pkgs.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
 
-      # Niri screensharing fixes.
+      # [2/2] Niri screensharing fixes.
       pkgs.xdg-desktop-portal-gnome
       pkgs.xdg-desktop-portal-gtk
     ];
@@ -29,9 +24,6 @@ in mkIf config.isDesktopNotWsl {
 
   environment.sessionVariables = {
     # Hint Electron apps to use Wayland.
-    NIXOS_OZONE_WL      = "1";
-    XDG_CURRENT_DESKTOP = "niri";
-    XDG_SESSION_TYPE    = "wayland";
-    XDG_SESSION_DESKTOP = "niri";
+    NIXOS_OZONE_WL = "1";
   };
 }
