1a70e4d53d8ac677be24515f0ba503b95f6332de
PlumJam <git@plumj.am>
2025-11-02 17:30:36 +0100

    qutebrowser: Improve config.

--- modules/common/qutebrowser.nix
+++ modules/common/qutebrowser.nix
@@ -1,5 +1,5 @@
 { lib, config, ... }: let
-  inherit (lib) enabled mkIf;
+  inherit (lib) enabled mkIf genAttrs const;
 in {
 
   home-manager.sharedModules = mkIf config.isDesktopNotWsl [{
@@ -8,22 +8,39 @@ in {
         tabs = {
           position             = "left";
           show                 = "always";
+          show_switching_delay = 3000;
           indicator.width      = 2;
           favicons.scale       = 0.8;
           mousewheel_switching = false;
           mode_on_change       = "restore";
+          title.format         = "{audio}{aligned_index}: {current_title}";
+          title.format_pinned  = "{audio}{aligned_index}: {current_title}";
+          undo_stack_size      = 1000;
         };
 
-        keyhint.delay     = 0;
-        completion.height = "25%";
-        completion.shrink = true;
-        messages.timeout  = 10000;
+        editor.command = ["ghostty" "-e" "hx" "{file}:{line}:{column}"];
+
+        completion = {
+          height                = "33%";
+          shrink                = true;
+          cmd_history_max_items = 100000;
+          use_best_match        = true;
+        };
+
+        content.cookies.accept = "no-unknown-3rdparty";
+
+        keyhint.delay             = 0;
+        messages.timeout          = 5000;
+        downloads.remove_finished = 30000;
 
         prompt.radius  = config.theme.radius.normal;
         keyhint.radius = config.theme.radius.normal;
         hints.radius   = config.theme.radius.normal;
 
         fonts.default_family = config.theme.font.mono.family;
+        fonts.web.family     = genAttrs
+          ["sans_serif" "serif" "standard"]
+          (const (config.theme.font.sans.name));
 
         colors.webpage.darkmode.enabled = config.theme.is_dark;
 
@@ -40,16 +57,27 @@ in {
           "scroll"
           "history"
           "tabs"
-          "text:qute "
+          # "text:qute"
+          "clock"
         ];
+      };
+
+      aliases = {
+        "bc" = "tab-close";
+        "rl" = "reload";
+        "bp" = "tab-prev";
+        "bn" = "tab-next";
       };
 
       keyBindings.normal = {
         "d"        = "nop";
-        "J"        = "tab-prev";
-        "K"        = "tab-next";
+        "f"        = "nop";
+        "gw"       = "hint";
+        "K"        = "tab-prev";
+        "J"        = "tab-next";
         "<Space>f" = "cmd-set-text -s :open -tr";
         "<Space>b" = "cmd-set-text -s :tab-select";
+        "<Ctrl+s>" = "config-cycle tabs.show always never";
       };
 
       extraConfig = ''
