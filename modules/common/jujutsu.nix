5e7c4c42825a3d8a916a4c750785df6950877ca2
PlumJam <git@plumj.am>
2025-11-08 15:15:01 +0100

    jujutsu: Script for pushing to 2+ remotes and fix indentation.

--- modules/common/jujutsu.nix
+++ modules/common/jujutsu.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, lib, keys, ... }: let
-  inherit (lib) enabled;
+{ config, pkgs, lib, ... }: let
+  inherit (lib) enabled mkAfter;
+
 in {
   environment.shellAliases = {
     j  = "jj";
@@ -13,112 +14,149 @@ in {
       programs.difftastic = enabled;
 
       programs.mergiraf = enabled;
+
+      programs.nushell.configFile.text = mkAfter /* nu */ ''
+        def --wrapped jpa [
+          --revisions (-r): string = "@" # The revision(s) to pass to `jj git push`
+          ...rest: string # Any other args to pass to `jj git push`
+        ] {
+          let remotes       = ["origin" "forgejo"]
+          let remotes_full  = (jj git remote list | lines | split column " " name url)
+          let remotes_names = ($remotes_full | get name)
+          for remote in $remotes {
+            if $remote in $remotes_names {
+              print $"(ansi purple)[Pusher](ansi rst) Pushing to ($remote)."
+              jj git push ...$rest --revisions $revisions --remote $remote
+            } else if $remote == "forgejo" {
+              print $"(ansi purple)[Pusher](ansi rst) Forgejo remote not found."
+              let input = (input --numchar 1 $"(ansi purple)[Pusher](ansi rst) Attempt to add forgejo remote? \(y/n\) ")
+              if ($input | str downcase | str starts-with "y") {
+                let origin_url = ($remotes_full | where name == "origin" | get url | first)
+                let repo_name = ($origin_url | split row "/" | last)
+                let forgejo_url = $"https://git.plumj.am/plumjam/($repo_name)"
+                print $"(ansi purple)[Pusher](ansi rst) Adding forgejo remote: ($forgejo_url)"
+                jj git remote add forgejo $forgejo_url
+                print $"(ansi purple)[Pusher](ansi rst) Added forgejo remote, pushing..."
+                jj git push ...$rest --revisions $revisions --remote $remote
+              } else {
+                print $"(ansi purple)[Pusher](ansi rst) Skipping forgejo remote setup."
+              }
+            } else {
+              print $"(ansi purple)[Pusher](ansi rst) Remote ($remote) not available, skipping."
+            }
+          }
+        }
+      '';
     }
     (homeArgs: let
       config' = homeArgs.config;
     in {
       # credit to https://github.com/rgbcube/ncc for most of this
       programs.jujutsu = enabled {
-      settings = {
-        user.name  = config'.programs.git.settings.user.name;
-        user.email = config'.programs.git.settings.user.email;
+        settings = {
+          user.name  = config'.programs.git.settings.user.name;
+          user.email = config'.programs.git.settings.user.email;
 
-      signing.key      = "${homeArgs.config.home.homeDirectory}/.ssh/id";
-      signing.backend  = "ssh";
-      signing.behavior = "own";
+          signing.key      = "${homeArgs.config.home.homeDirectory}/.ssh/id";
+          signing.backend  = "ssh";
+          signing.behavior = "own";
 
-      ui.conflict-marker-style = "snapshot";
-      ui.default-command       = "lg";
-      ui.diff-editor           = ":builtin";
-      ui.diff-formatter        = [ "difft" "--color" "always" "$left" "$right" ];
-      ui.editor                = config.environment.variables.EDITOR;
-      ui.graph-style           = "square";
-      ui.movement.edit         = true;
-      ui.pager                 = ":builtin";
+          ui.conflict-marker-style = "snapshot";
+          ui.default-command       = "lg";
+          ui.diff-editor           = ":builtin";
+          ui.diff-formatter        = [ "difft" "--color" "always" "$left" "$right" ];
+          ui.editor                = config.environment.variables.EDITOR;
+          ui.graph-style           = "square";
+          ui.movement.edit         = true;
+          ui.pager                 = ":builtin";
 
-      snapshot.max-new-file-size = "10MiB";
+          snapshot.max-new-file-size = "10MiB";
 
-      lazyjj.highlight-color = "#f2e5bc";
+          lazyjj.highlight-color = "#f2e5bc";
 
-      git.sign-on-push        = true; # sign in bulk on push
-      git.auto-local-bookmark = true;
+          git.sign-on-push        = true; # sign in bulk on push
+          git.auto-local-bookmark = true;
 
-      aliases.".." = [ "edit" "@-" ];
-      aliases.",," = [ "edit" "@+" ];
+          git.subprocess = true;
 
-      aliases.a = [ "abandon" ];
+          git.fetch = [ "origin" "forgejo" ];
+          git.push  = "origin";
 
-      aliases.c  = [ "commit" ];
-      aliases.ci = [ "commit" "--interactive" ];
+          aliases.".." = [ "edit" "@-" ];
+          aliases.",," = [ "edit" "@+" ];
 
-      aliases.e = [ "edit" ];
+          aliases.a = [ "abandon" ];
 
-      aliases.fetch = [ "git" "fetch" ];
-      aliases.f     = [ "git" "fetch" ];
+          aliases.c  = [ "commit" ];
+          aliases.ci = [ "commit" "--interactive" ];
 
-      aliases.r = [ "rebase" ];
+          aliases.e = [ "edit" ];
 
-      aliases.res         = [ "resolve" ];
-      aliases.resolve-ast = [ "resolve" "--tool" "mergiraf" ];
-      aliases.resa        = [ "resolve-ast" ];
+          aliases.fetch = [ "git" "fetch" ];
+          aliases.f     = [ "git" "fetch" ];
 
-      aliases.s = [ "split" ];
-      aliases.sm = [ "split" "--message" ];
+          aliases.r = [ "rebase" ];
 
-      aliases.sq   = [ "squash" ];
-      aliases.sqf  = [ "squash" "--from" ];
-      aliases.sqi  = [ "squash" "--interactive" ];
-      aliases.sqm  = [ "squash" "--message" ];
-      aliases.sqmi = [ "squash" "--interactive" "--message" ];
+          aliases.res         = [ "resolve" ];
+          aliases.resolve-ast = [ "resolve" "--tool" "mergiraf" ];
+          aliases.resa        = [ "resolve-ast" ];
 
-      aliases.sh = [ "show" ];
+          aliases.s = [ "split" ];
+          aliases.sm = [ "split" "--message" ];
 
-      aliases.tug = [ "bookmark" "move" "--from" "closest(@-)" "--to" "closest_pushable(@)" ];
-      aliases.t   = [ "tug" ];
+          aliases.sq   = [ "squash" ];
+          aliases.sqf  = [ "squash" "--from" ];
+          aliases.sqi  = [ "squash" "--interactive" ];
+          aliases.sqm  = [ "squash" "--message" ];
+          aliases.sqmi = [ "squash" "--interactive" "--message" ];
 
-      aliases.push = [ "git" "push" ];
-      aliases.p    = [ "git" "push" ];
+          aliases.sh = [ "show" ];
 
-      aliases.init  = [ "git" "init" "--colocate" ];
-      aliases.i     = [ "git" "init" "--colocate" ];
+          aliases.tug = [ "bookmark" "move" "--from" "closest(@-)" "--to" "closest_pushable(@)" ];
+          aliases.t   = [ "tug" ];
 
-      aliases.clone = [ "git" "clone" "--colocate" ];
-      aliases.cl    = [ "git" "clone" "--colocate" ];
+          aliases.push = [ "git" "push" ];
+          aliases.p    = [ "git" "push" ];
 
-      aliases.d = [ "diff" ];
+          aliases.init  = [ "git" "init" "--colocate" ];
+          aliases.i     = [ "git" "init" "--colocate" ];
 
-      aliases.l   = [ "log" ];
-      aliases.la  = [ "log" "--revisions" "::" ];
-      aliases.ls  = [ "log" "--summary" ];
-      aliases.lsa = [ "log" "--summary" "--revisions" "::" ];
-      aliases.lp  = [ "log" "--patch" ];
-      aliases.lpa = [ "log" "--patch" "--revisions" "::" ];
-      aliases.lg  = [ "log" "--summary" "--no-pager" "--limit=4" ];
-      aliases.el  = [ "evolog" ];
-      aliases.ol  = [ "op" "log" ];
+          aliases.clone = [ "git" "clone" "--colocate" ];
+          aliases.cl    = [ "git" "clone" "--colocate" ];
 
-      revset-aliases."closest(to)"          = "heads(::to & bookmarks())";
-      revset-aliases."closest_pushable(to)" = "heads(::to & ~description(exact:\"\") & (~empty() | merges()))";
+          aliases.d = [ "diff" ];
 
-      revsets.log = "present(@) | present(trunk()) | ancestors(remote_bookmarks().. | @.., 8)";
+          aliases.l   = [ "log" ];
+          aliases.la  = [ "log" "--revisions" "::" ];
+          aliases.ls  = [ "log" "--summary" ];
+          aliases.lsa = [ "log" "--summary" "--revisions" "::" ];
+          aliases.lp  = [ "log" "--patch" ];
+          aliases.lpa = [ "log" "--patch" "--revisions" "::" ];
+          aliases.lg  = [ "log" "--summary" "--no-pager" "--limit=4" ];
+          aliases.el  = [ "evolog" ];
+          aliases.ol  = [ "op" "log" ];
 
-      templates.draft_commit_description = /* python */ ''
-          concat(
-            coalesce(description, "\n"),
-            surround(
-              "\nJJ: This commit contains the following changes:\n", "",
-              indent("JJ:     ", diff.stat(72)),
-            ),
-            "\nJJ: ignore-rest\n",
-            diff.git(),
-          )
-        '';
+          revset-aliases."closest(to)"          = "heads(::to & bookmarks())";
+          revset-aliases."closest_pushable(to)" = "heads(::to & ~description(exact:\"\") & (~empty() | merges()))";
 
-      templates.git_push_bookmark = /* python */ ''
-        "change/PlumJam-" ++ change_id.short()
-      '';
+          revsets.log = "present(@) | present(trunk()) | ancestors(remote_bookmarks().. | @.., 8)";
 
-      };
+          templates.draft_commit_description = /* python */ ''
+            concat(
+              coalesce(description, "\n"),
+              surround(
+                "\nJJ: This commit contains the following changes:\n", "",
+                indent("JJ:     ", diff.stat(72)),
+              ),
+              "\nJJ: ignore-rest\n",
+              diff.git(),
+            )
+          '';
+
+          templates.git_push_bookmark = /* python */ ''
+            "change/PlumJam-" ++ change_id.short()
+          '';
+        };
       };
     })
   ];
