c02cbf1351b8fcc9e748f0471b5bc68e3d1d6587
PlumJam <git@plumj.am>
2025-11-08 15:11:35 +0100

    nix: Only use distributed builds on weaker systems.

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,32 +1,35 @@
 { self, config, inputs, lib, ... }: let
-  inherit (lib) attrsToList concatStringsSep const disabled filter filterAttrs flip id isType mapAttrs mapAttrsToList merge optionalAttrs optionals removeAttrs;
+  inherit (lib) attrsToList concatStringsSep const disabled filter filterAttrs flip id isType mapAttrs mapAttrsToList merge optionalAttrs optionals removeAttrs mkIf;
+  inherit (config.networking) hostName;
 
   registryMap = inputs
     |> filterAttrs (const <| isType "flake");
 in {
-  nix.distributedBuilds = true;
-  nix.buildMachines = self.nixosConfigurations
+  nix.distributedBuilds = mkIf (hostName != "yuzu") true; # No distributed builds for powerful desktop.
+  nix.buildMachines     = mkIf (hostName != "yuzu") (
+    self.nixosConfigurations
     |> attrsToList
     |> filter ({ name, value }:
-    name != config.networking.hostName &&
-    value.config.users.users ? build)
+      name != config.networking.hostName &&
+      value.config.users.users ? build)
     |> map ({ name, value }: {
-      hostName = name;
-      maxJobs = 25; # This is handled by remote anyway so not sure what difference it makes..
-      protocol = "ssh-ng";
-      sshUser = "build";
-      sshKey = "/root/.ssh/id";
+      hostName          = name;
+      maxJobs           = 25; # This is handled by remote anyway so not sure what difference it makes..
+      protocol          = "ssh-ng";
+      sshUser           = "build";
+      sshKey            = "/root/.ssh/id";
       supportedFeatures = [ "benchmark" "big-parallel" "kvm" "nixos-test" ];
-      system = value.config.nixpkgs.hostPlatform.system;
-    });
+      system            = value.config.nixpkgs.hostPlatform.system;
+    })
+  );
 
   nix.channel = disabled;
 
   nix.gc = merge {
-    automatic = true;
-    options = "--delete-older-than 7d";
+    automatic  = true;
+    options    = "--delete-older-than 7d";
   } <| optionalAttrs (config.isLinux) {
-    dates = "weekly";
+    dates      = "weekly";
     persistent = true;
   };
 
