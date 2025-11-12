472820ff2b631fa14c58d8db885717a9e991c091
PlumJam <git@plumj.am>
2025-11-12 15:54:42 +0100

    hosts.{kiwi,plum}: Remove GitHub runners.

--- hosts/plum/git-runners/default.nix
+++ hosts/plum/git-runners/default.nix
@@ -1,18 +1,6 @@
 { pkgs, lib, config, ... }: let
   inherit (lib) enabled;
 in {
-  age.secrets.githubRunnerToken.rekeyFile = ./github-runner-token.age;
-  services.github-runners.${config.networking.hostName} = enabled {
-    name          = config.networking.hostName;
-    tokenFile     = config.age.secrets.githubRunnerToken.path;
-    url           = "https://github.com/plumj-am/docpad";
-    user          = "build";
-    extraPackages = [
-      pkgs.curl
-      pkgs.xz
-    ];
-  };
-
   age.secrets.forgejoRunnerToken.rekeyFile = ./forgejo-runner-token.age;
   services.gitea-actions-runner = {
     package = pkgs.forgejo-runner;
