4a246f8ad2db503d254cd29dff633066367aef98
PlumJam <git@plumj.am>
2025-11-28 16:48:23 +0100

    hyprlock: Adjust layout and fallback background colour.

--- modules/linux/hyprlock.nix
+++ modules/linux/hyprlock.nix
@@ -16,19 +16,22 @@ in mkIf config.isDesktopNotWsl {
           immediate_render   = true;
         };
 
+
         animations.enabled = false;
 
         background = [{
+          color       = "rgba(0,0,0,1.0)";
           path        = "screenshot";
           brightness  = 0.4;
           blur_passes = 3;
         }];
 
         input-field = [{
-          size        = "400, 50";
-          position    = "0, -80";
-          monitor     = yuzuMonitor;
-          font_family = config.theme.font.mono.family;
+          size          = "400, 50";
+          position      = "0, -80";
+          monitor       = yuzuMonitor;
+          font_family   = config.theme.font.mono.family;
+          fade_on_empty = false;
         }];
 
         label = [
@@ -64,7 +67,7 @@ in mkIf config.isDesktopNotWsl {
             text        = ''cmd[update:1000] echo "<b> Uptime: $(cat /proc/uptime | awk '{printf "%.0f", $1}' | awk '{days=int($1/86400); hours=int(($1%86400)/3600); minutes=int(($1%3600)/60); seconds=$1%60; printf "%dd %dh %dm %ds", days, hours, minutes, seconds}') </b>"'';
             font_size   = 12;
             font_family = config.theme.font.mono.family;
-            position    = "0, -100";
+            position    = "0, -200";
             halign      = "center";
             valign      = "center";
           }
