61a62be288ced32ab4dd6bbe47fc3d62ba06f0a5
PlumJam <git@plumj.am>
2025-11-28 16:48:23 +0100

    power-menu: Add a hibernate option.

--- modules/linux/power-menu.nix
+++ modules/linux/power-menu.nix
@@ -2,7 +2,7 @@
   inherit (lib) mkIf;
 
   power-menu = pkgs.writeShellScriptBin "power-menu" ''
-    choice=$(echo -e "Shutdown\nReboot\nSleep\nLock" | ${pkgs.fuzzel}/bin/fuzzel --dmenu --prompt "Power: ")
+    choice=$(echo -e "Shutdown\nReboot\nSleep\nHibernate\nLock" | ${pkgs.fuzzel}/bin/fuzzel --dmenu --prompt "Power: ")
 
     case "$choice" in
       "Shutdown")
@@ -13,6 +13,9 @@
         ;;
       "Sleep")
         hyprlock --quiet & systemctl suspend
+        ;;
+      "Hibernate")
+        hyprlock --quiet & systemctl hibernate
         ;;
       "Lock")
         hyprlock --quiet --grace 60
