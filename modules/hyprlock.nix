9af00b30c3672e5c65af16b5e236a6293030e665
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    dendritic: Migrate hypr tools.

--- modules/hyprlock.nix
+++ modules/hyprlock.nix
@@ -1,17 +1,15 @@
 {
-  config.flake.modules.homeModules.hyprlock =
-    { config, lib, ... }:
+  config.flake.modules.hjem.hyprlock =
+    { lib, theme, ... }:
     let
-      inherit (lib) mkIf;
-
       yuzuMonitor = # mkIf (config.networking.hostName == "yuzu")
         "DP-2";
     in
     {
-      programs.hyprlock = {
+      rum.programs.hyprlock = {
         enable = true;
         settings = {
-          font-name = "${config.theme.font.sans.name} ${toString config.theme.font.size.small}";
+          font-name = "${theme.font.sans.name} ${toString theme.font.size.small}";
 
           general = {
             hide_cursor = true;
@@ -35,7 +33,7 @@
               size = "400, 50";
               position = "0, -80";
               monitor = yuzuMonitor;
-              font_family = config.theme.font.mono.family;
+              font_family = theme.font.mono.family;
               fade_on_empty = false;
             }
           ];
@@ -45,7 +43,7 @@
               monitor = yuzuMonitor;
               text = ''cmd[update:18000000] echo "<b> "$(date +'%A, %-d %B %Y')" </b>"'';
               font_size = 24;
-              font_family = config.theme.font.mono.family;
+              font_family = theme.font.mono.family;
               position = "0, -150";
               halign = "center";
               valign = "top";
@@ -54,7 +52,7 @@
               monitor = yuzuMonitor;
               text = ''cmd[update:1000] echo "<b><big> $(date +"%H:%M:%S") </big></b>"'';
               font_size = 64;
-              font_family = config.theme.font.mono.family;
+              font_family = theme.font.mono.family;
               position = "0, 0";
               halign = "center";
               valign = "center";
@@ -63,7 +61,7 @@
               monitor = yuzuMonitor;
               text = "$USER";
               font_size = 16;
-              font_family = config.theme.font.mono.family;
+              font_family = theme.font.mono.family;
               position = "0, 100";
               halign = "center";
               valign = "bottom";
@@ -72,7 +70,7 @@
               monitor = yuzuMonitor;
               text = ''cmd[update:1000] echo "<b> Uptime: $(cat /proc/uptime | awk '{printf "%.0f", $1}' | awk '{days=int($1/86400); hours=int(($1%86400)/3600); minutes=int(($1%3600)/60); seconds=$1%60; printf "%dd %dh %dm %ds", days, hours, minutes, seconds}') </b>"'';
               font_size = 12;
-              font_family = config.theme.font.mono.family;
+              font_family = theme.font.mono.family;
               position = "0, -200";
               halign = "center";
               valign = "center";
