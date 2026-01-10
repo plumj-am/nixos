51d27bca33fd436b8f6d724e58f8c6280f9cb8ab
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    dendritic: Migrate GTK, cursor and QT configs.

---
+++ modules/desktop-gui.nix
@@ -0,0 +1,40 @@
+{
+  config.flake.modules.nixos.desktop-gui =
+    { pkgs, config, ... }:
+    {
+      qt = {
+        enable = true;
+
+        style = config.theme.qt.name;
+      };
+
+      programs.dconf.enable = true;
+
+      environment.systemPackages = [
+        pkgs.bibata-cursors
+      ];
+
+      environment.sessionVariables = {
+        XCURSOR_THEME = "Bibata-Modern-Classic";
+        XCURSOR_SIZE = "24";
+      };
+    };
+
+  config.flake.modules.hjem.desktop-gui =
+    { theme, ... }:
+    let
+      packages = [ theme.gtk.package ];
+    in
+    {
+      rum.misc.gtk = {
+        inherit packages;
+        enable = true;
+        settings = {
+          application-prefers-dark-theme = true;
+          font-name = "${theme.font.sans.name} ${toString theme.font.size.small}";
+          theme-name = theme.gtk.name;
+          icon-theme-name = theme.icons.name;
+        };
+      };
+    };
+}
