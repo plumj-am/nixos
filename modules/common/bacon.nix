35f5f4fbcc60f561dda188eb9608326d8890cf5a
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    dendritic: Migrate Bacon and Rust configs.

--- modules/common/bacon.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }: let
-	inherit (lib) enabled mkIf;
-in
-{
-  home-manager.sharedModules = [{
-    programs.bacon = mkIf config.isDesktop enabled;
-  }];
-}
---
+++ modules/rust.nix
@@ -0,0 +1,80 @@
+let
+  commonModule =
+    { pkgs, ... }:
+    {
+      environment.sessionVariables = {
+        CARGO_NET_GIT_FETCH_WITH_CLI = "true";
+      };
+
+      environment.systemPackages = [
+        pkgs.cargo-binstall
+        pkgs.cargo-nextest
+        pkgs.dioxus-cli
+      ];
+    };
+
+  rustDesktop =
+    { pkgs, inputs }:
+    {
+      environment.systemPackages = [
+        (inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.complete.withComponents [
+          # Nightly.
+          "cargo"
+          "clippy"
+          "miri"
+          "rustc"
+          "rust-analyzer"
+          "rustfmt"
+          "rust-std"
+          "rust-src"
+        ])
+        pkgs.bacon
+        pkgs.cargo-careful
+        pkgs.cargo-deny
+        pkgs.cargo-generate
+        pkgs.cargo-machete
+        pkgs.cargo-workspaces
+        pkgs.cargo-outdated
+        pkgs.kondo
+      ];
+    };
+in
+{
+  config.flake.modules.nixos.rust =
+    { pkgs, inputs, ... }:
+    {
+      environment.systemPackages = [
+        (inputs.fenix.packages.${pkgs.stdenv.hostPlatform.system}.complete.withComponents [
+          # Nightly.
+          "cargo"
+          "clippy"
+          "miri"
+          "rustc"
+          "rustfmt"
+          "rust-std"
+          "rust-src"
+        ])
+      ];
+    }
+    // commonModule { inherit pkgs; };
+
+  config.flake.modules.nixos.rust-desktop =
+    { pkgs, inputs, ... }: rustDesktop { inherit pkgs inputs; } // commonModule { inherit pkgs; };
+
+  config.flake.modules.darwin.rust-desktop =
+    {
+      lib,
+      pkgs,
+      inputs,
+      ...
+    }:
+    let
+      inherit (lib.strings) makeLibraryPath;
+    in
+    rustDesktop { inherit pkgs inputs; }
+    // {
+      environment.variables = {
+        LIBRARY_PATH = makeLibraryPath [ pkgs.libiconv ];
+      };
+    };
+}
