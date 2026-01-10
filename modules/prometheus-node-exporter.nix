9236874cfb15d6077c915b725ab0c0ed9a951fd1
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    dendritic: Migrate Prometheus node exporter config.

---
+++ modules/prometheus-node-exporter.nix
@@ -0,0 +1,13 @@
+{
+  config.flake.modules.nixos.prometheus-node-exporter = {
+    services.prometheus.exporters.node = {
+      enable = true;
+      enabledCollectors = [
+        "processes"
+        "systemd"
+        "hwmon"
+      ];
+      listenAddress = "[::]";
+    };
+  };
+}
