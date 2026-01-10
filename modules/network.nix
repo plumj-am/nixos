54f187f621bcc4959f5cf51d1d0ce6229ecad935
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    dendritic: Migrate network config.

--- modules/network.nix
+++ modules/network.nix
@@ -1,50 +1,90 @@
-{ config, lib, ... }: let
-  inherit (lib) enabled mkIf types;
-in {
-  # Named "network" to avoid conflicts with the "networking" options.
-  # Not sure if it would even be a problem but oh well.
-  options.network = {
-    enable = lib.mkEnableOption "network";
+{
+  config.flake.modules.nixos.network =
+    { config, lib, ... }:
+    let
+      inherit (lib)
+        mkIf
+        types
+        mkOption
+        mkDefault
+        ;
+    in
+    {
+      # Named "network" to avoid conflicts with the "networking" options.
+      # Not sure if it would even be a problem but oh well.
+      options.network = {
 
-    hostName = lib.mkOption {
-      type = types.str;
-      example = "yuzu";
-      description = "Host's unique name";
+        hostName = mkOption {
+          type = types.str;
+          example = "yuzu";
+          description = "Host's unique name";
+        };
+
+        interfaces = mkOption {
+          type = with types; listOf str;
+          example = [ "ts0" ];
+          default = [ "ts0" ];
+          description = "Network interfaces";
+        };
+
+        domain = mkOption {
+          type = with types; nullOr str;
+          default = null;
+          example = "example.com";
+          description = "Network domain (optional)";
+        };
+
+        tcpPorts = mkOption {
+          type = with types; listOf (either int str);
+          default = [ 22 ];
+          example = [
+            22
+            80
+            443
+          ];
+          description = "TCP ports to allow through the firewall";
+        };
+      };
+
+      config = {
+        networking.networkmanager = {
+          enable = true;
+          wifi.powersave = false;
+        };
+        programs.nm-applet.enable = true;
+        users.users.jam.extraGroups = [ "networkmanager" ];
+
+        networking.hostName = config.network.hostName;
+
+        networking.domain = mkIf (config.network.domain != null) config.network.domain;
+
+        networking.firewall = {
+          enable = true;
+          trustedInterfaces = config.network.interfaces;
+          allowedTCPPorts = config.network.tcpPorts;
+        };
+
+        networking.useDHCP = mkDefault true;
+        networking.interfaces = { };
+      };
     };
 
-    interfaces = lib.mkOption {
-      type = with types; listOf str;
-      example = [ "ts0" ];
-      default = [ "ts0" ];
-      description = "Network interfaces";
+  config.flake.modules.darwin.network =
+    { config, lib, ... }:
+    let
+      inherit (lib) types mkOption;
+    in
+    {
+      # Named "network" to avoid conflicts with the "networking" options.
+      # Not sure if it would even be a problem but oh well.
+      options.network = {
+        hostName = mkOption {
+          type = types.str;
+          example = "yuzu";
+          description = "Host's unique name";
+        };
+      };
+
+      config.networking.hostName = config.network.hostName;
     };
-
-    domain = lib.mkOption {
-      type = with types; nullOr str;
-      default = null;
-      example = "example.com";
-      description = "Network domain (optional)";
-    };
-
-    tcpPorts = lib.mkOption {
-      type = with types; listOf (either int str);
-      default = [ 22 ];
-      example = [ 22 80 443 ];
-      description = "TCP ports to allow through the firewall";
-    };
-  };
-
-  config = {
-    networking.hostName = config.network.hostName;
-
-    networking.domain = mkIf (config.network.domain != null) config.network.domain;
-
-    networking.firewall = enabled {
-      trustedInterfaces = config.network.interfaces;
-      allowedTCPPorts   = config.network.tcpPorts;
-    };
-
-    networking.useDHCP    = lib.mkDefault true;
-    networking.interfaces = {};
-  };
 }
