d39a15f11c95a8ad9447be77be7aa4a039d11865
PlumJam <git@plumj.am>
2026-01-10 22:42:52 +0100

    chore: Remove `hosts/` and `lib/`.

--- lib/default.nix
+++
@@ -1,7 +0,0 @@
-inputs: self: super: let
-  filesystem = import ./filesystem.nix inputs self super;
-  option     = import ./option.nix     inputs self super;
-  system     = import ./system.nix     inputs self super;
-  values     = import ./values.nix     inputs self super;
-  dendritic  = import ./dendritic.nix  inputs self super;
-in filesystem // option // system // values // dendritic // { inherit inputs; }
