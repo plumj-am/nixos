511ef146d077b721417f05b6cc69f9d7daf10fc6
PlumJam <git@plumj.am>
2026-01-13 16:08:48 +0100

    dendritic: Migrate acme and nginx configs.

---
+++ modules/acme.nix
@@ -0,0 +1,29 @@
+{
+  config.flake.modules.nixos.acme =
+    { config, lib, ... }:
+    let
+      inherit (config.network) domain;
+      inherit (lib) mkValue;
+    in
+    {
+      options.security.acme.users = mkValue [ ];
+
+      config.users.groups.acme.members = config.security.acme.users;
+
+      config.security.acme = {
+        acceptTerms = true;
+
+        defaults = {
+          environmentFile = config.age.secrets.acmeEnvironment.path;
+          dnsProvider = "cloudflare";
+          dnsResolver = "1.1.1.1";
+          email = "security@${domain}";
+        };
+
+        certs.${domain} = {
+          extraDomainNames = [ "*.${domain}" ];
+          group = "acme";
+        };
+      };
+    };
+}
