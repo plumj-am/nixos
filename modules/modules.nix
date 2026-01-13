76044c75fcba0056f2fb0e2afd1113c75add0eb5
PlumJam <git@plumj.am>
2026-01-13 16:08:48 +0100

    nix: Add a formatter flake output for easy formatting.

--- modules/modules.nix
+++ modules/modules.nix
@@ -1,4 +1,14 @@
 { inputs, ... }:
 {
-  imports = [ inputs.parts.flakeModules.modules ];
+  imports = [
+    inputs.parts.flakeModules.modules
+
+    {
+      perSystem =
+        { inputs', ... }:
+        {
+          _module.args.pkgs = inputs'.os.legacyPackages;
+        };
+    }
+  ];
 }
