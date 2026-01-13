e626b0970fee7e1e998fa9af42abe0346e45cec5
PlumJam <git@plumj.am>
2026-01-13 16:08:48 +0100

    hardware: Remove duplicate fileSystem definitions.
    Now completely handled in disks.nix.

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -25,26 +25,6 @@
         "sd_mod"
       ];
 
-      fileSystems."/" = {
-        device = "/dev/disk/by-label/nixos";
-        fsType = "ext4";
-      };
-
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-label/BOOT";
-        fsType = "vfat";
-        options = [
-          "fmask=0077"
-          "dmask=0077"
-        ];
-      };
-
-      swapDevices = [
-        {
-          device = "/dev/disk/by-label/SWAP";
-        }
-      ];
-
       environment.systemPackages = [
         pkgs.lshw # Hardware info.
         pkgs.usbutils # USB device info.
