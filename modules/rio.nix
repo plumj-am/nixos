109d36ac3d14a5de4a5ff1882a539e372db60fb9
PlumJam <git@plumj.am>
2026-01-13 16:08:48 +0100

    rio: Add package.

---
+++ modules/rio.nix
@@ -0,0 +1,11 @@
+{
+  # TODO: Configuration.
+  # No point configuring yet because it still doesn't work for some reason.
+  # config.flake.modules.hjem.rio =
+  #   { pkgs, ... }:
+  #   {
+  #     packages = [
+  #       pkgs.rio
+  #     ];
+  #   };
+}
