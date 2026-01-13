2b0fdb4cc689139b77a19ca27b767f1d209130ea
PlumJam <git@plumj.am>
2026-01-13 16:08:48 +0100

    yazi: Add config.

---
+++ modules/yazi.nix
@@ -0,0 +1,35 @@
+{
+  config.flake.modules.hjem.yazi =
+    { lib, isDesktop, ... }:
+    let
+      inherit (lib.modules) mkIf;
+    in
+    mkIf isDesktop {
+      rum.programs.yazi = {
+        enable = true;
+
+        settings = {
+          mgr = {
+            ratio = [
+              2
+              2
+              4
+            ];
+            show_hidden = true;
+            show_symlink = false;
+            scrolloff = 5;
+          };
+
+          preview = {
+            wrap = "no";
+            tab_size = 3;
+            image_delay = 0;
+            image_filter = "lanczos3";
+            image_quality = 90;
+          };
+
+          input.cursor_blink = false;
+        };
+      };
+    };
+}
