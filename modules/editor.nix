133e34cf6d28d4296bcbd9ef9c0ca269e99ad281
PlumJam <git@plumj.am>
2026-02-02 21:49:39 +0100

    helix: Enable additional nixd options for remaining hosts.

--- modules/editor.nix
+++ modules/editor.nix
@@ -360,8 +360,9 @@
                 yuzu.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.yuzu.options'';
                 plum.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.plum.options'';
                 kiwi.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.kiwi.options'';
-                # date.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.date.options'';
-                # pear.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.pear.options'';
+                sloe.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.sloe.options'';
+                date.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.date.options'';
+                pear.expr = ''(builtins.getFlake "/home/jam/nixos").nixosConfigurations.pear.options'';
                 lime.expr = ''(builtins.getFlake "/home/jam/nixos").darwinConfigurations.lime.options'';
                 flake-parts.expr = ''(builtins.getFlake "/home/jam/nixos").debug.options'';
                 flake-parts2.expr = ''(builtins.getFlake "/home/jam/nixos").currentSystem.options'';
