d99c34303bc8d23ac1c39240ce0869a2e788c6fc
PlumJam <git@plumj.am>
2026-02-05 23:48:59 +0100

    ai: Fix some config types.

--- modules/ai.nix
+++ modules/ai.nix
@@ -86,15 +86,16 @@
 
           env = {
             ANTHROPIC_BASE_URL = "https://api.z.ai/api/anthropic";
-            API_TIMEOUT_MS = "3000000";
+            API_TIMEOUT_MS = 3000000;
 
             ANTHROPIC_DEFAULT_HAIKU_MODEL = "glm-4.5-air";
             ANTHROPIC_DEFAULT_SONNET_MODEL = "glm-4.7";
             ANTHROPIC_DEFAULT_OPUS_MODEL = "glm-4.7";
 
-            CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR = "1";
-            CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
-            DISABLE_NON_ESSENTIAL_MODEL_CALLS = "1";
+            CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR = 1;
+            CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = 1;
+            CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS = 1;
+            DISABLE_NON_ESSENTIAL_MODEL_CALLS = 1;
           };
 
           hooks = {
