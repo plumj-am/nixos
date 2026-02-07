f375b32cc511949ab6207a92ac465effe2240761
PlumJam <git@plumj.am>
2026-02-07 17:10:30 +0100

    {hosts,aspects}: Clean up and adjust how aspects are used.
    Also renamed the modules.nix file to aspects.nix to better reflect its
    purpose.

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,8 +1,6 @@
 { inputs, ... }:
 let
   inherit (inputs.self) mkConfig;
-
-  specialArgs = { inherit inputs; };
 in
 {
   flake-file.inputs = {
@@ -24,43 +22,51 @@ in
     };
   };
 
+  # Yuzu | desktop | x86_64-linux | NixOS
   flake.nixosConfigurations.yuzu = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      desktopModules
+      aspectsBase
 
       ai
       ai-extra
+      audio
       app-launcher
       boot-systemd
       desktop-gui
-      disable-nano
+      desktop-tools
       discord-gui
       discord-tui
+      disks-normal
       disks-extra-swap
-      env
+      disks-extra-zram-swap
       games
       gammastep
-      git
-      helix
+      graphics
+      hardware-desktop
       helix-extra
       hyprlock
+      jujutsu-extra
       kitty
+      linux-kernel-zen
       mprocs
-      notifications
       niri
-      # nix-distributed-builds
-      nix-settings
+      notifications
       nix-settings-extra-desktop
       object-storage
-      openssh
-      jujutsu
+      packages-extra-linux
+      packages-extra-gui
+      packages-extra-cli
+      peripherals
+      power-menu
+      process-management
+      quickshell
+      rust-desktop
       rss-tui
-      shell
-      shell-tools
-      ssh
+      sudo-extra-desktop
+      theme-extra-fonts
+      theme-extra-scripts
       waybar
       yazi
       zellij
@@ -82,18 +88,57 @@ in
     ];
   };
 
+  # Date | laptop/server | x86_64-linux | NixOS
   flake.nixosConfigurations.date = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      desktopModules
+      aspectsBase
 
+      ai
+      ai-extra
+      app-launcher
+      audio
       boot-systemd
+      desktop-gui
+      desktop-tools
+      discord-gui
+      discord-tui
+      disks-normal
+      disks-extra-swap
+      disks-extra-zram-swap
       forgejo-action-runner
-      nix-distributed-builds
+      games
+      gammastep
+      graphics
+      hardware-desktop
+      helix-extra
+      hyprlock
+      jujutsu-extra
+      kitty
+      linux-kernel-zen
+      mprocs
+      niri
+      notifications
+      nix-settings-extra-desktop
       nix-distributed-builder
+      nix-distributed-builds
       object-storage
+      packages-extra-linux
+      packages-extra-gui
+      packages-extra-cli
+      peripherals
+      power-menu
+      process-management
+      quickshell
+      rust-desktop
+      rss-tui
+      sudo-extra-desktop
+      theme-extra-fonts
+      theme-extra-scripts
+      waybar
+      yazi
+      zellij
       {
         config = mkConfig inputs "date" "x86_64-linux" "desktop" {
           age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzfoVKZDyiyyMiX1JRFaaTELspG25MlLNq0kI2AANTa root@date";
@@ -125,21 +170,20 @@ in
     ];
   };
 
+  # Pear | WSL | x86_64-linux | NixOS-WSL
   flake.nixosConfigurations.pear = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
+      aspectsBase
 
       desktop-tools
       jujutsu-extra
       linux-kernel-zen
-      packages-extra-desktop
       nix-distributed-builds
       nix-distributed-builder
       # object-storage
-      scratchpads
-      sudo-desktop
+      sudo-extra-desktop
       wsl
       {
         config = mkConfig inputs "pear" "x86_64-linux" "wsl" {
@@ -156,26 +200,30 @@ in
     ];
   };
 
+  # Plum | server | x86_64-linux | NixOS
   flake.nixosConfigurations.plum = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      serverModules
+      aspectsBase
 
       acme
       boot-grub
       cgit
       disks-disko
       forgejo
+      freshrss-server
       forgejo-action-runner
       goatcounter
+      linux-kernel-latest
       nginx
       nix-distributed-builds
       nix-distributed-builder
       object-storage
       opengist
-      freshrss-server
+      prometheus-node-exporter
+      rust
+      sudo-extra-server
       uptime-kuma
       website-personal
       { hardware.facter.reportPath = ./facter/plum.json; }
@@ -227,21 +275,25 @@ in
     ];
   };
 
+  # Kiwi | server | x86_64-linux | NixOS
   flake.nixosConfigurations.kiwi = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      serverModules
+      aspectsBase
 
       acme
       boot-grub
       disks-disko
       forgejo-action-runner
+      linux-kernel-latest
       nginx
       nix-distributed-builds
       nix-distributed-builder
       object-storage
+      prometheus-node-exporter
+      rust
+      sudo-extra-server
       website-dr-radka
       {
         config = mkConfig inputs "kiwi" "x86_64-linux" "server" {
@@ -269,19 +321,23 @@ in
     ];
   };
 
+  # Sloe | server | x86_64-linux | NixOS
   flake.nixosConfigurations.sloe = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      serverModules
+      aspectsBase
 
       boot-grub
       disks-disko
       forgejo-action-runner
+      linux-kernel-latest
       nix-distributed-builds
       nix-distributed-builder
       object-storage
+      prometheus-node-exporter
+      rust
+      sudo-extra-server
       {
         config = mkConfig inputs "sloe" "x86_64-linux" "server" {
 
@@ -301,19 +357,23 @@ in
     ];
   };
 
+  # Blackwell | server | x86_64-linux | NixOS
   flake.nixosConfigurations.blackwell = inputs.os.lib.nixosSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.nixos; [
-      commonModules
-      serverModules
+      aspectsBase
 
       boot-grub
       disks-disko
       forgejo-action-runner
+      linux-kernel-latest
       nix-distributed-builds
       nix-distributed-builder
       object-storage
+      prometheus-node-exporter
+      rust
+      sudo-extra-server
       {
         config = mkConfig inputs "blackwell" "x86_64-linux" "server" {
 
@@ -331,26 +391,15 @@ in
     ];
   };
 
+  # Lime | Macbook | x86_64-linux | nix-darwin
   flake.darwinConfigurations.lime = inputs.os-darwin.lib.darwinSystem {
-    inherit specialArgs;
+    specialArgs = { inherit inputs; };
 
     modules = with inputs.self.modules.darwin; [
+      aspectsBase
+
       app-launcher
-      disable-nix-documentation
-      hjem
-      keys
-      lib
-      network
-      openssh
-      packages
       rust-desktop
-      secret-manager
-      sudo
-      system
-      tailscale
-      theme
-      unfree
-      users
       {
         config = {
           platform = "aarch64-darwin";
