077f98a644a88b7a6ef9eb61b19a89343e07219b
PlumJam <git@plumj.am>
2026-02-07 16:02:02 +0100

    home: Remove specialArgs and collected modules.
    Finally I don't need specialArgs...
    
    Still need to update all existing hosts to use the new system.

--- modules/home.nix
+++ modules/home.nix
@@ -1,18 +1,6 @@
+{ lib, ... }:
 let
-  commonSpecialArgs = config: inputs: {
-    inherit inputs;
-    inherit (config.age) secrets;
-    inherit (config.network) hostName;
-    inherit (config)
-      myLib
-      isDesktop
-      isServer
-      isWsl
-      isLinux
-      isDarwin
-      ;
-  };
-
+  inherit (lib.lists) singleton;
 in
 {
   flake-file.inputs = {
@@ -25,53 +13,14 @@ in
   };
 
   flake.modules.nixos.hjem =
+    { inputs, ... }:
     {
-      lib,
-      inputs,
-      config,
-      ...
-    }:
-    let
-      inherit (lib.attrsets) attrValues;
-
-      hjemModules = inputs.self.modules.hjem |> attrValues;
-    in
-    {
-      imports = [
-        inputs.hjem.nixosModules.default
-        {
-          hjem.extraModules = hjemModules;
-
-          hjem.specialArgs = commonSpecialArgs config inputs // {
-            inherit (config) theme;
-          };
-        }
-      ];
-
+      imports = singleton inputs.hjem.nixosModules.default;
     };
 
   flake.modules.darwin.hjem =
+    { inputs, ... }:
     {
-      lib,
-      inputs,
-      config,
-      ...
-    }:
-    let
-      inherit (lib.attrsets) attrValues;
-
-      hjemModules = inputs.self.modules.hjem |> attrValues;
-    in
-    {
-      imports = [
-        inputs.hjem.darwinModules.default
-        {
-          hjem.extraModules = hjemModules;
-
-          hjem.specialArgs = commonSpecialArgs config inputs // {
-            inherit (config) theme;
-          };
-        }
-      ];
+      imports = singleton inputs.hjem.darwinModules.default;
     };
 }
