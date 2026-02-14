e932647051c2fb37af937b80e025d7768a21208d
PlumJam <git@plumj.am>
2026-02-14 21:22:29 +0100

    hosts.kiwi: Set ownership for dr-radka secrets.

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -325,7 +325,11 @@ in
           age.secrets = {
             forgejoRunnerToken.rekeyFile = ../secrets/plum-forgejo-runner-token.age;
             acmeEnvironment.rekeyFile = ../secrets/acme-environment.age;
-            drRadkaEnvironment.rekeyFile = ../secrets/kiwi-dr-radka-environment.age;
+            drRadkaEnvironment = {
+              rekeyFile = ../secrets/kiwi-dr-radka-environment.age;
+              owner = "dr-radka";
+              group = "dr-radka";
+            };
             nixStoreKey.rekeyFile = ../secrets/kiwi-nix-store-key.age;
           };
 
