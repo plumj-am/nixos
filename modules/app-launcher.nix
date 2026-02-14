2aeb33a33c29016f4f78f14bd4a817a0ea8a7029
PlumJam <git@plumj.am>
2026-02-14 21:22:29 +0100

    deadnix: Comment out fuzzel config.

--- modules/app-launcher.nix
+++ modules/app-launcher.nix
@@ -1,52 +1,52 @@
 let
-  fuzzelBase =
-    {
-      pkgs,
-      config,
-      lib,
-      ...
-    }:
-    let
-      inherit (lib.lists) singleton;
-      inherit (config) theme;
+  # fuzzelBase =
+  #   {
+  #     pkgs,
+  #     config,
+  #     lib,
+  #     ...
+  #   }:
+  #   let
+  #     inherit (lib.lists) singleton;
+  #     inherit (config) theme;
 
-      settings = with theme; {
-        main = {
-          icon-theme = icons.name;
-          font = "${font.sans.name}:size=${toString font.size.normal}";
-          layer = "overlay";
-          prompt = ''"❯ "'';
-          terminal = "kitty";
+  #     settings = with theme; {
+  #       main = {
+  #         icon-theme = icons.name;
+  #         font = "${font.sans.name}:size=${toString font.size.normal}";
+  #         layer = "overlay";
+  #         prompt = ''"❯ "'';
+  #         terminal = "kitty";
 
-          horizontal-pad = padding.small;
-          vertical-pad = padding.small;
-        };
+  #         horizontal-pad = padding.small;
+  #         vertical-pad = padding.small;
+  #       };
 
-        colors = {
-          background = colors.base00 + "FF";
-          text = colors.base07 + "FF";
-          match = colors.base0A + "FF";
-          selection = colors.base0A + "88"; # Lower opacity.
-          selection-text = colors.base07 + "FF";
-          selection-match = colors.base08 + "FF";
-          border = colors.base0A + "FF";
-        };
+  #       colors = {
+  #         background = colors.base00 + "FF";
+  #         text = colors.base07 + "FF";
+  #         match = colors.base0A + "FF";
+  #         selection = colors.base0A + "88"; # Lower opacity.
+  #         selection-text = colors.base07 + "FF";
+  #         selection-match = colors.base08 + "FF";
+  #         border = colors.base0A + "FF";
+  #       };
 
-        border = {
-          radius = radius.big;
-          width = border.small;
-        };
-      };
+  #       border = {
+  #         radius = radius.big;
+  #         width = border.small;
+  #       };
+  #     };
 
-      ini = pkgs.formats.ini { };
-    in
-    {
-      hjem.extraModules = singleton {
-        packages = singleton pkgs.fuzzel;
+  #     ini = pkgs.formats.ini { };
+  #   in
+  #   {
+  #     hjem.extraModules = singleton {
+  #       packages = singleton pkgs.fuzzel;
 
-        xdg.config.files."fuzzel/fuzzel.ini".source = ini.generate "fuzzel.ini" settings;
-      };
-    };
+  #       xdg.config.files."fuzzel/fuzzel.ini".source = ini.generate "fuzzel.ini" settings;
+  #     };
+  #   };
 
   tofiBase =
     {
