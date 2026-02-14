9aca7d57dc72e41d08118a45a96f4a5cb748b692
PlumJam <git@plumj.am>
2026-02-14 21:22:29 +0100

    dr-radka: Switch to deno and drop sanity support.

--- modules/website-dr-radka.nix
+++ modules/website-dr-radka.nix
@@ -3,9 +3,11 @@
     {
       config,
       pkgs,
+      lib,
       ...
     }:
     let
+      inherit (lib.meta) getExe;
       inherit (config.networking) domain;
       inherit (config.myLib) merge;
 
@@ -35,7 +37,7 @@
           User = app_user;
           Group = app_group;
           WorkingDirectory = build_dir;
-          ExecStart = "${pkgs.bun}/bin/bun run ./index.js";
+          ExecStart = "${getExe pkgs.deno} run --allow-all ./index.js";
           Restart = "always";
           RestartSec = 5;
           EnvironmentFile = config.age.secrets.drRadkaEnvironment.path;
@@ -57,7 +59,7 @@
           HOST = "127.0.0.1";
           ORIGIN = "https://${domain}";
         };
-        path = [ pkgs.bun ];
+        path = [ pkgs.deno ];
       };
 
       services.nginx = {
@@ -75,30 +77,30 @@
           locations."/" = {
             proxyPass = "http://127.0.0.1:${toString app_port}";
             proxyWebsockets = true;
-            extraConfig = ''
-              proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Proto $scheme;
-              proxy_cache_bypass $http_upgrade;
+            extraConfig = # nginx
+              ''
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+                proxy_cache_bypass $http_upgrade;
 
-              # override csp for built app requirements and maintain security headers
-              proxy_hide_header Content-Security-Policy;
-              add_header Content-Security-Policy "script-src 'self' 'unsafe-inline' 'unsafe-eval' ${domain} *.${domain} cdn.jsdelivr.net unpkg.com *.posthog.com *.sanity.io *.googletagmanager.com *.google-analytics.com analytics.plumj.am; object-src 'self' ${domain} *.${domain}; base-uri 'self'; frame-ancestors 'self' dr-radka.sanity.studio *.sanity.io; form-action 'self' ${domain} *.${domain}; font-src 'self' ${domain} *.${domain} cdn.jsdelivr.net; connect-src 'self' ${domain} *.${domain} unpkg.com *.posthog.com *.sanity.io *.googletagmanager.com *.google-analytics.com analytics.plumj.am; img-src 'self' ${domain} *.${domain} unpkg.com *.tile.openstreetmap.org *.sanity.io cdn.sanity.io www.googletagmanager.com data:;" always;
-              # need to fix because I can't access nested routes in sanity presentation mode
-              add_header X-Frame-Options DENY always;
-              add_header X-Content-Type-Options nosniff always;
-              add_header X-XSS-Protection "1; mode=block" always;
-              add_header Permissions-Policy "camera=(), geolocation=(), payment=(), usb=()" always;
-              add_header Referrer-Policy no-referrer always;
-            '';
+                # override csp for built app requirements and maintain security headers
+                proxy_hide_header Content-Security-Policy;
+                add_header Content-Security-Policy "script-src 'self' 'unsafe-inline' 'unsafe-eval' ${domain} *.${domain} cdn.jsdelivr.net unpkg.com *.posthog.com *.googletagmanager.com *.google-analytics.com analytics.plumj.am; object-src 'self' ${domain} *.${domain}; base-uri 'self'; frame-ancestors 'self'; form-action 'self' ${domain} *.${domain}; font-src 'self' ${domain} *.${domain} cdn.jsdelivr.net; connect-src 'self' ${domain} *.${domain} unpkg.com *.posthog.com *.googletagmanager.com *.google-analytics.com analytics.plumj.am; img-src 'self' ${domain} *.${domain} unpkg.com *.tile.openstreetmap.org www.googletagmanager.com data:;" always;
+                add_header X-Frame-Options DENY always;
+                add_header X-Content-Type-Options nosniff always;
+                add_header X-XSS-Protection "1; mode=block" always;
+                add_header Permissions-Policy "camera=(), geolocation=(), payment=(), usb=()" always;
+                add_header Referrer-Policy no-referrer always;
+              '';
           };
         };
       };
 
       environment.systemPackages = [
-        pkgs.nodejs_22
-        pkgs.bun
+        pkgs.nodejs_25
+        pkgs.deno
       ];
     };
 }
