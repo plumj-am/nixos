15248d6dc027710133675e4e8b3753f6c59326da
PlumJam <git@plumj.am>
2026-02-14 21:22:29 +0100

    networking: Remove useless option.

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,20 +2,9 @@
   flake.modules.nixos.networking =
     { lib, ... }:
     let
-      inherit (lib.types) nullOr str;
-      inherit (lib.options) mkOption;
       inherit (lib) mkDefault;
     in
     {
-      options.network = {
-        domain = mkOption {
-          type = nullOr str;
-          default = null;
-          example = "example.com";
-          description = "Network domain (optional)";
-        };
-      };
-
       config = {
         networking.networkmanager = {
           enable = true;
