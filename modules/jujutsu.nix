608ea6a104a9bbd4276b4b4e25f015a1e67bdd83
PlumJam <git@plumj.am>
2026-03-10 23:25:06 +0100

    jujutsu: Move config inside hjem and fix signing path.
    It didn't work on Darwin before and needs to be in hjem to access
    `config.directory`. Only realising now because I'm finally using Lime
    again for a bit lol.

--- modules/jujutsu.nix
+++ modules/jujutsu.nix
@@ -33,233 +33,6 @@ let
       inherit (config) theme;
 
       toml = pkgs.formats.toml { };
-      jjConfig = {
-        user.name = "PlumJam";
-        user.email = "git@plumj.am";
-
-        signing.key = "/home/jam/.ssh/id";
-        signing.backend = "ssh";
-        signing.behavior = "own";
-
-        ui.conflict-marker-style = "snapshot";
-        ui.default-command = "lg";
-        ui.diff-editor = ":builtin";
-        ui.diff-formatter = [
-          "difft"
-          "--color"
-          "always"
-          "$left"
-          "$right"
-        ];
-        ui.editor = config.environment.variables.EDITOR;
-        ui.graph.style = "curved";
-        ui.movement.edit = true;
-        ui.pager = ":builtin";
-
-        snapshot.max-new-file-size = "10MiB";
-
-        lazyjj.highlight-color = "#${theme.colors.base02}";
-
-        git = {
-          sign-on-push = true; # Sign in bulk on push.
-          subprocess = true;
-          private-commits = "description('wip:*') | description('private:*')"; # Prevent pushing WIP commits.
-        };
-
-        remotes.origin.auto-track-bookmarks = "glob:*";
-
-        git.fetch = [
-          "origin"
-          "rad"
-        ];
-        git.push = "origin";
-
-        aliases.".." = [
-          "edit"
-          "@-"
-        ];
-        aliases.",," = [
-          "edit"
-          "@+"
-        ];
-
-        aliases.a = [ "abandon" ];
-
-        aliases.b = [ "bookmark" ];
-        aliases.bs = [
-          "bookmark"
-          "set"
-        ];
-        aliases.bc = [
-          "bookmark"
-          "create"
-        ];
-
-        aliases.c = [ "commit" ];
-        aliases.ci = [
-          "commit"
-          "--interactive"
-        ];
-
-        aliases.e = [ "edit" ];
-
-        aliases.fetch = [
-          "git"
-          "fetch"
-        ];
-        aliases.f = [
-          "git"
-          "fetch"
-        ];
-
-        aliases.r = [ "rebase" ];
-
-        aliases.res = [ "resolve" ];
-        aliases.resolve-ast = [
-          "resolve"
-          "--tool"
-          "mergiraf"
-        ];
-        aliases.resa = [ "resolve-ast" ];
-
-        aliases.s = [ "split" ];
-        aliases.sm = [
-          "split"
-          "--message"
-        ];
-
-        aliases.sq = [ "squash" ];
-        aliases.sqf = [
-          "squash"
-          "--from"
-        ];
-        aliases.sqi = [
-          "squash"
-          "--interactive"
-        ];
-        aliases.sqm = [
-          "squash"
-          "--message"
-        ];
-        aliases.sqmi = [
-          "squash"
-          "--interactive"
-          "--message"
-        ];
-
-        aliases.sh = [ "show" ];
-
-        aliases.tug = [
-          "bookmark"
-          "move"
-          "--from"
-          "closest(@-)"
-          "--to"
-          "closest_pushable(@)"
-        ];
-        aliases.t = [ "tug" ];
-
-        aliases.push = [
-          "git"
-          "push"
-        ];
-        aliases.p = [
-          "git"
-          "push"
-        ];
-        aliases.pb = [
-          "git"
-          "push"
-          "--bookmark"
-        ];
-
-        aliases.init = [
-          "git"
-          "init"
-          "--colocate"
-        ];
-        aliases.i = [
-          "git"
-          "init"
-          "--colocate"
-        ];
-
-        aliases.clone = [
-          "git"
-          "clone"
-          "--colocate"
-        ];
-        aliases.cl = [
-          "git"
-          "clone"
-          "--colocate"
-        ];
-
-        aliases.d = [ "diff" ];
-        aliases.ds = [ "diff --stat" ];
-
-        aliases.l = [ "log" ];
-        aliases.la = [
-          "log"
-          "--revisions"
-          "::"
-        ];
-        aliases.ls = [
-          "log"
-          "--summary"
-        ];
-        aliases.lsa = [
-          "log"
-          "--summary"
-          "--revisions"
-          "::"
-        ];
-        aliases.lp = [
-          "log"
-          "--patch"
-        ];
-        aliases.lpa = [
-          "log"
-          "--patch"
-          "--revisions"
-          "::"
-        ];
-        aliases.lg = [
-          "log"
-          "--summary"
-          "--no-pager"
-          "--limit=4"
-        ];
-        aliases.el = [ "evolog" ];
-        aliases.ol = [
-          "op"
-          "log"
-        ];
-
-        revset-aliases."closest(to)" = "heads(::to & bookmarks())";
-        revset-aliases."closest_pushable(to)" =
-          "heads(::to & ~description(exact:\"\") & (~empty() | merges()))";
-
-        revsets.log = "present(@) | present(trunk()) | ancestors(remote_bookmarks().. | @.., 8)";
-
-        templates.draft_commit_description = # python
-          ''
-            concat(
-              coalesce(description, "\n"),
-              surround(
-                "\nJJ: This commit contains the following changes:\n", "",
-                indent("JJ:     ", diff.stat(72)),
-              ),
-              "\nJJ: ignore-rest\n",
-              diff.git(),
-            )
-          '';
-
-        templates.git_push_bookmark = # python
-          ''
-            "patch/PlumJam-" ++ change_id.short()
-          '';
-      };
 
       # TODO: Fix sub-menu selection bg colour (press "l" on revision to view files)
       jjuiConfig = {
@@ -270,10 +43,239 @@ let
       };
     in
     {
-      hjem.extraModules = singleton {
-        xdg.config.files."jj/config.toml".source = toml.generate "jj-config.toml" jjConfig;
-        xdg.config.files."jjui/config.toml".source = toml.generate "jjui-config.toml" jjuiConfig;
-      };
+      hjem.extraModules = singleton (
+        { config, ... }:
+        {
+          xdg.config.files."jj/config.toml".source = toml.generate "jj-config.toml" {
+            user.name = "PlumJam";
+            user.email = "git@plumj.am";
+
+            signing.key = "${config.directory}/.ssh/id";
+            signing.backend = "ssh";
+            signing.behavior = "own";
+
+            ui.conflict-marker-style = "snapshot";
+            ui.default-command = "lg";
+            ui.diff-editor = ":builtin";
+            ui.diff-formatter = [
+              "difft"
+              "--color"
+              "always"
+              "$left"
+              "$right"
+            ];
+            ui.editor = config.environment.variables.EDITOR;
+            ui.graph.style = "curved";
+            ui.movement.edit = true;
+            ui.pager = ":builtin";
+
+            snapshot.max-new-file-size = "10MiB";
+
+            lazyjj.highlight-color = "#${theme.colors.base02}";
+
+            git = {
+              sign-on-push = true; # Sign in bulk on push.
+              subprocess = true;
+              private-commits = "description('wip:*') | description('private:*')"; # Prevent pushing WIP commits.
+            };
+
+            remotes.origin.auto-track-bookmarks = "glob:*";
+
+            git.fetch = [
+              "origin"
+              "rad"
+            ];
+            git.push = "origin";
+
+            aliases.".." = [
+              "edit"
+              "@-"
+            ];
+            aliases.",," = [
+              "edit"
+              "@+"
+            ];
+
+            aliases.a = [ "abandon" ];
+
+            aliases.b = [ "bookmark" ];
+            aliases.bs = [
+              "bookmark"
+              "set"
+            ];
+            aliases.bc = [
+              "bookmark"
+              "create"
+            ];
+
+            aliases.c = [ "commit" ];
+            aliases.ci = [
+              "commit"
+              "--interactive"
+            ];
+
+            aliases.e = [ "edit" ];
+
+            aliases.fetch = [
+              "git"
+              "fetch"
+            ];
+            aliases.f = [
+              "git"
+              "fetch"
+            ];
+
+            aliases.r = [ "rebase" ];
+
+            aliases.res = [ "resolve" ];
+            aliases.resolve-ast = [
+              "resolve"
+              "--tool"
+              "mergiraf"
+            ];
+            aliases.resa = [ "resolve-ast" ];
+
+            aliases.s = [ "split" ];
+            aliases.sm = [
+              "split"
+              "--message"
+            ];
+
+            aliases.sq = [ "squash" ];
+            aliases.sqf = [
+              "squash"
+              "--from"
+            ];
+            aliases.sqi = [
+              "squash"
+              "--interactive"
+            ];
+            aliases.sqm = [
+              "squash"
+              "--message"
+            ];
+            aliases.sqmi = [
+              "squash"
+              "--interactive"
+              "--message"
+            ];
+
+            aliases.sh = [ "show" ];
+
+            aliases.tug = [
+              "bookmark"
+              "move"
+              "--from"
+              "closest(@-)"
+              "--to"
+              "closest_pushable(@)"
+            ];
+            aliases.t = [ "tug" ];
+
+            aliases.push = [
+              "git"
+              "push"
+            ];
+            aliases.p = [
+              "git"
+              "push"
+            ];
+            aliases.pb = [
+              "git"
+              "push"
+              "--bookmark"
+            ];
+
+            aliases.init = [
+              "git"
+              "init"
+              "--colocate"
+            ];
+            aliases.i = [
+              "git"
+              "init"
+              "--colocate"
+            ];
+
+            aliases.clone = [
+              "git"
+              "clone"
+              "--colocate"
+            ];
+            aliases.cl = [
+              "git"
+              "clone"
+              "--colocate"
+            ];
+
+            aliases.d = [ "diff" ];
+            aliases.ds = [ "diff --stat" ];
+
+            aliases.l = [ "log" ];
+            aliases.la = [
+              "log"
+              "--revisions"
+              "::"
+            ];
+            aliases.ls = [
+              "log"
+              "--summary"
+            ];
+            aliases.lsa = [
+              "log"
+              "--summary"
+              "--revisions"
+              "::"
+            ];
+            aliases.lp = [
+              "log"
+              "--patch"
+            ];
+            aliases.lpa = [
+              "log"
+              "--patch"
+              "--revisions"
+              "::"
+            ];
+            aliases.lg = [
+              "log"
+              "--summary"
+              "--no-pager"
+              "--limit=4"
+            ];
+            aliases.el = [ "evolog" ];
+            aliases.ol = [
+              "op"
+              "log"
+            ];
+
+            revset-aliases."closest(to)" = "heads(::to & bookmarks())";
+            revset-aliases."closest_pushable(to)" =
+              "heads(::to & ~description(exact:\"\") & (~empty() | merges()))";
+
+            revsets.log = "present(@) | present(trunk()) | ancestors(remote_bookmarks().. | @.., 8)";
+
+            templates.draft_commit_description = # python
+              ''
+                concat(
+                  coalesce(description, "\n"),
+                  surround(
+                    "\nJJ: This commit contains the following changes:\n", "",
+                    indent("JJ:     ", diff.stat(72)),
+                  ),
+                  "\nJJ: ignore-rest\n",
+                  diff.git(),
+                )
+              '';
+
+            templates.git_push_bookmark = # python
+              ''
+                "patch/PlumJam-" ++ change_id.short()
+              '';
+          };
+          xdg.config.files."jjui/config.toml".source = toml.generate "jjui-config.toml" jjuiConfig;
+        }
+      );
     };
 in
 {
