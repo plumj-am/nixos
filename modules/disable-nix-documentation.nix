5220a4a2d828c353c7e8c820b5179466bc87d960
PlumJam <git@plumj.am>
2026-03-11 14:18:10 +0100

    nix-docs: Deduplicate config.
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/disable-nix-documentation.nix
+++ modules/disable-nix-documentation.nix
@@ -1,17 +1,13 @@
+let
+  documentationBase = {
+    documentation = {
+      doc.enable = false;
+      info.enable = false;
+      man.enable = true;
+    };
+  };
+in
 {
-  flake.modules.nixos.disable-nix-documentation = {
-    documentation = {
-      doc.enable = false;
-      info.enable = false;
-      man.enable = true;
-    };
-  };
-
-  flake.modules.darwin.disable-nix-documentation = {
-    documentation = {
-      doc.enable = false;
-      info.enable = false;
-      man.enable = true;
-    };
-  };
+  flake.modules.nixos.disable-nix-documentation = documentationBase;
+  flake.modules.darwin.disable-nix-documentation = documentationBase;
 }
