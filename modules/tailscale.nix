a180942af7284d893a72ad47630d8f74d9d8895e
PlumJam <git@plumj.am>
2026-03-15 02:57:31 +0100

    tailscale: Add auth key for simpler setup.
    Note that it does needs to be recreated every 90 days.
    
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,13 +1,18 @@
 {
   flake.modules.nixos.tailscale =
+    { config, ... }:
     let
       interface = "ts0";
       domains = [ "taild29fec.ts.net" ];
     in
     {
+      age.secrets.tailscaleAuthKey.rekeyFile = ../secrets/tailscale-auth-key.age;
+
       services.resolved.settings.Resolve.Domains = domains;
       services.tailscale = {
         enable = true;
+
+        authKeyFile = config.age.secrets.tailscaleAuthKey.path;
 
         useRoutingFeatures = "both";
         interfaceName = interface;
