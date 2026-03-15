59feca6c2ab6bf2127ae2619ae4f9c5b46ade275
PlumJam <git@plumj.am>
2026-03-15 02:57:31 +0100

    hjem: Add a single module option to avoid needing list.
    It can be used like `hjem.extraModule {}` instead of
    `hjem.extraModules = [{}]` or `hjem.extraModules = singleton {};
    
    I never create more than 1 module inside these lists. An issue related to
    this: <https://github.com/feel-co/hjem/issues/124>
    
    
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/hjem.nix
+++ modules/hjem.nix
@@ -1,17 +1,46 @@
 { lib, ... }:
 let
   inherit (lib.lists) singleton;
+  inherit (lib.types) deferredModule nullOr;
 in
 {
   flake.modules.nixos.hjem =
-    { inputs, ... }:
+    { inputs, config, ... }:
     {
       imports = singleton inputs.hjem.nixosModules.default;
+
+      options.hjem.extraModule = lib.mkOption {
+        type = nullOr deferredModule;
+        default = null;
+        description = ''
+          Single module to be evaluated as a part of the users module
+          inside `config.hjem.users.<username>`. Use this instead of
+          `extraModules` when you only have one module to add.
+        '';
+      };
+
+      config.hjem.extraModules = lib.mkIf (config.hjem.extraModule != null) [
+        config.hjem.extraModule
+      ];
     };
 
   flake.modules.darwin.hjem =
-    { inputs, ... }:
+    { inputs, config, ... }:
     {
       imports = singleton inputs.hjem.darwinModules.default;
+
+      options.hjem.extraModule = lib.mkOption {
+        type = nullOr deferredModule;
+        default = null;
+        description = ''
+          Single module to be evaluated as a part of the users module
+          inside `config.hjem.users.<username>`. Use this instead of
+          `extraModules` when you only have one module to add.
+        '';
+      };
+
+      config.hjem.extraModules = lib.mkIf (config.hjem.extraModule != null) [
+        config.hjem.extraModule
+      ];
     };
 }
