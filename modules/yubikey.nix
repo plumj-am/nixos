eca93d6ea7c51fed4650bd1b1e843878eb407e68
PlumJam <git@plumj.am>
2026-03-29 23:38:37 +0200

    yubikey: Deduplicate config.
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -1,62 +1,44 @@
+let
+  yubikeyCommon =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [
+        pkgs.yubikey-personalization
+        pkgs.age-plugin-yubikey
+      ];
+
+      services.udev.packages = [
+        pkgs.yubikey-personalization
+      ];
+
+      security.pam.services = {
+        login = {
+          u2fAuth = true;
+          enableGnomeKeyring = true;
+        };
+        sudo.u2fAuth = true;
+        su.u2fAuth = true;
+        sshd.u2fAuth = true;
+      };
+
+      services.pcscd.enable = true;
+      programs.yubikey-manager.enable = true;
+      programs.yubikey-touch-detector = {
+        enable = true;
+        libnotify = true;
+      };
+    };
+in
 {
   flake.modules.nixos.yubikey =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
+    let
+      inherit (lib.lists) singleton;
+    in
     {
-      environment.systemPackages = [
-        pkgs.yubikey-personalization
-        pkgs.yubioath-flutter
-        pkgs.age-plugin-yubikey
-
-      ];
-
-      services.udev.packages = [
-        pkgs.yubikey-personalization
-      ];
-
-      security.pam.services = {
-        login = {
-          u2fAuth = true;
-          enableGnomeKeyring = true;
-        };
-        sudo.u2fAuth = true;
-        su.u2fAuth = true;
-        sshd.u2fAuth = true;
-      };
-
-      services.pcscd.enable = true;
-      programs.yubikey-manager.enable = true;
-      programs.yubikey-touch-detector = {
-        enable = true;
-        libnotify = true;
-      };
+      imports = singleton yubikeyCommon;
+      environment.systemPackages = singleton pkgs.yubioath-flutter;
     };
 
-  flake.modules.darwin.yubikey =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = [
-        pkgs.yubikey-personalization
-        pkgs.age-plugin-yubikey
-      ];
-
-      services.udev.packages = [
-        pkgs.yubikey-personalization
-      ];
-      security.pam.services = {
-        login = {
-          u2fAuth = true;
-          enableGnomeKeyring = true;
-        };
-        sudo.u2fAuth = true;
-        su.u2fAuth = true;
-        sshd.u2fAuth = true;
-      };
-
-      services.pcscd.enable = true;
-      programs.yubikey-manager.enable = true;
-      programs.yubikey-touch-detector = {
-        enable = true;
-        libnotify = true;
-      };
-    };
+  flake.modules.darwin.yubikey = yubikeyCommon;
 }
