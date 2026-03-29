3c4e000e34398c98f4c89be06b9804ba0d564f92
PlumJam <git@plumj.am>
2026-03-29 23:38:37 +0200

    hjem: Deduplicate config.
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/hjem.nix
+++ modules/hjem.nix
@@ -1,46 +1,30 @@
-{ lib, ... }:
+{ inputs, lib, ... }:
 let
   inherit (lib.lists) singleton;
   inherit (lib.types) deferredModule nullOr;
+
+  mkHjemModule =
+    hjemModule:
+    { config, ... }:
+    {
+      imports = singleton hjemModule;
+
+      options.hjem.extraModule = lib.mkOption {
+        type = nullOr deferredModule;
+        default = null;
+        description = ''
+          Single module to be evaluated as a part of the users module
+          inside `config.hjem.users.<username>`. Use this instead of
+          `extraModules` when you only have one module to add.
+        '';
+      };
+
+      config.hjem.extraModules = lib.mkIf (config.hjem.extraModule != null) [
+        config.hjem.extraModule
+      ];
+    };
 in
 {
-  flake.modules.nixos.hjem =
-    { inputs, config, ... }:
-    {
-      imports = singleton inputs.hjem.nixosModules.default;
-
-      options.hjem.extraModule = lib.mkOption {
-        type = nullOr deferredModule;
-        default = null;
-        description = ''
-          Single module to be evaluated as a part of the users module
-          inside `config.hjem.users.<username>`. Use this instead of
-          `extraModules` when you only have one module to add.
-        '';
-      };
-
-      config.hjem.extraModules = lib.mkIf (config.hjem.extraModule != null) [
-        config.hjem.extraModule
-      ];
-    };
-
-  flake.modules.darwin.hjem =
-    { inputs, config, ... }:
-    {
-      imports = singleton inputs.hjem.darwinModules.default;
-
-      options.hjem.extraModule = lib.mkOption {
-        type = nullOr deferredModule;
-        default = null;
-        description = ''
-          Single module to be evaluated as a part of the users module
-          inside `config.hjem.users.<username>`. Use this instead of
-          `extraModules` when you only have one module to add.
-        '';
-      };
-
-      config.hjem.extraModules = lib.mkIf (config.hjem.extraModule != null) [
-        config.hjem.extraModule
-      ];
-    };
+  flake.modules.nixos.hjem = mkHjemModule inputs.hjem.nixosModules.default;
+  flake.modules.darwin.hjem = mkHjemModule inputs.hjem.darwinModules.default;
 }
