fe526b7118b7e9a443b3c5a225a31729edc264dd
PlumJam <git@plumj.am>
2026-05-01 04:02:14 +0200

    llama-cpp: Update mkUnslothQwen helper and add more quants.
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/ai/local.nix
+++ modules/ai/local.nix
@@ -19,16 +19,19 @@
       };
 
       mkUnslothQwen =
-        name: rest:
+        { name, ctx, ... }:
         {
           hf-repo = "unsloth/${name}";
           jinja = "on";
           chat-template-kwargs = ''{"preserve_thinking": true}'';
           no-mmproj-offload = "on";
+          ctx-size = ctx;
+
+          cache-type-k = "q8_0";
+          cache-type-v = "q8_0";
         }
         # WARN: Ngram can break tool calls, apparently.
         // ngram
-        // rest
         // cpuMoeOffload;
     in
     {
@@ -49,66 +52,65 @@
         modelsPreset = {
           # tps | ctx max | ctx max local
           # 8.4 | 262144  |
-          "unsloth/Qwen3.6-35B-A3B:UD-IQ3_XXS" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:UD-IQ3_XXS" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "131072";
+          "unsloth/Qwen3.6-35B-A3B:UD-IQ3_XXS" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:UD-IQ3_XXS";
+            ctx = "131072";
           };
 
           # tps | ctx max | ctx max local
           # 8.3 | 262144  |
-          "unsloth/Qwen3.6-35B-A3B:UD-IQ3_S" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:UD-IQ3_S" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "131072";
+          "unsloth/Qwen3.6-35B-A3B:UD-IQ3_S" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:UD-IQ3_S";
+            ctx = "131072";
           };
 
           # tps  | ctx max | ctx max local
           # 11.3 | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:UD-IQ4_XS" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:UD-IQ4_XS" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "131072";
+          "unsloth/Qwen3.6-35B-A3B:UD-IQ4_XS" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:UD-IQ4_XS";
+            ctx = "131072";
           };
 
           # tps | ctx max | ctx local max
           # 0   | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:UD-IQ4_NL_XL" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:UD-IQ4_NL_XL" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "131072"; # 262144 max - untested
+          "unsloth/Qwen3.6-35B-A3B:UD-IQ4_NL_XL" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:UD-IQ4_NL_XL";
+            ctx = "131072"; # 262144 max - untested
           };
 
           # tps  | ctx max | ctx max local
           # 25.0 | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:Q4_K_S" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:Q4_K_S" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "262144"; # 262144 max - untested
+          "unsloth/Qwen3.6-35B-A3B:Q4_K_S" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:Q4_K_S";
+            ctx = "262144"; # 262144 max - untested
           };
 
           # tps  | ctx max | ctx max local
           # 23.3 | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:Q4_K_XL" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:Q4_K_XL" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "262144"; # 262144 max - untested
+          "unsloth/Qwen3.6-35B-A3B:Q4_K_XL" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:Q4_K_XL";
+            ctx = "262144"; # 262144 max - untested
           };
 
           # tps  | ctx max | ctx max local
           # 21.0 | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:Q5_K_S" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:Q5_K_S" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "262144"; # 262144 max - untested
+          "unsloth/Qwen3.6-35B-A3B:Q5_K_S" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:Q5_K_S";
+            ctx = "262144"; # 262144 max - untested
           };
 
           # tps  | ctx max | ctx max local
-          # 0    | 262144  | 0
-          "unsloth/Qwen3.6-35B-A3B:Q6_K" = mkUnslothQwen "Qwen3.6-35B-A3B-GGUF:Q6_K" {
-            cache-type-k = "q8_0";
-            cache-type-v = "q8_0";
-            ctx-size = "262144"; # 262144 max - untested
+          # 14.7 | 262144  | 0
+          "unsloth/Qwen3.6-35B-A3B:Q6_K" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:Q6_K";
+            ctx = "262144"; # 262144 max - untested
+          };
+
+          # tps  | ctx max | ctx max local
+          # 16.4 | 262144  | 0
+          "unsloth/Qwen3.6-35B-A3B:Q8_K_XL" = mkUnslothQwen {
+            name = "Qwen3.6-35B-A3B-GGUF:Q8_K_XL";
+            ctx = "262144"; # 262144 max - untested
           };
 
           # tps | ctx max | ctx max local
