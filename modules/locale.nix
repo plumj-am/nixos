682db71f326ab3247b2efee744180871408dcd55
PlumJam <git@plumj.am>
2026-05-28 20:44:40 +0200

    locale: add fallbacks for timezone detection
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,6 +1,15 @@
 {
-  flake.modules.nixos.locale = {
-    time.timeZone = "Europe/Warsaw";
-    i18n.defaultLocale = "en_US.UTF-8";
-  };
+  flake.modules.nixos.locale =
+    let
+      tz = "Europe/Warsaw";
+    in
+    {
+      time.timeZone = tz;
+      i18n.defaultLocale = "en_US.UTF-8";
+
+      # Fallbacks for different detection methods.
+      environment.etc."timezone".text = tz;
+      environment.sessionVariables.TZ = tz;
+      systemd.globalEnvironment.TZ = tz;
+    };
 }
