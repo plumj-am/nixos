d6c3149e830330d2ba2e486a1ba458b50ef93ba1
PlumJam <git@plumj.am>
2026-06-11 17:30:17 +0200

    swap: automate swapfile creation
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/swap.nix
+++ modules/swap.nix
@@ -1,10 +1,16 @@
 {
   flake.modules.nixos.swapfile =
-    { lib, config, ... }:
+    {
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
     let
       inherit (lib.lists) singleton;
       inherit (lib.options) mkOption;
-      inherit (lib.types) int str;
+      inherit (lib.modules) mkMerge mkIf;
+      inherit (lib.types) nullOr int str;
 
       cfg = config.systemInfo.disks;
     in
@@ -18,20 +24,50 @@
 
             };
             size = mkOption {
-              type = int;
+              type = nullOr int;
               default = null;
             };
           };
         };
       };
 
-      config = {
-        boot.zswap.enable = true;
-        swapDevices = singleton {
-          device = cfg.swap.file.path;
-          size = cfg.swap.file.size;
-        };
-      };
+      config = mkMerge [
+        {
+          boot.zswap.enable = true;
+          swapDevices = singleton {
+            device = cfg.swap.file.path;
+          };
+        }
+        (mkIf (cfg.swap.file.size != null) {
+          systemd.services.create-swapfile =
+            let
+              swapDevUnit = "swap-${lib.replaceStrings [ "/" ] [ "-" ] cfg.swap.file.path}.swap";
+            in
+            {
+              description = "Create swapfile";
+              before = [ swapDevUnit ];
+              wantedBy = [ swapDevUnit ];
+              serviceConfig = {
+                Type = "oneshot";
+                RemainAfterExit = true;
+              };
+              script =
+                let
+                  path = cfg.swap.file.path;
+                  size = cfg.swap.file.size;
+                in
+                ''
+                  if ! test -f "${path}"; then
+                    ${pkgs.util-linux}/bin/fallocate -l ${toString size}M "${path}"
+                    ${pkgs.coreutils}/bin/chmod 0600 "${path}"
+                    ${pkgs.util-linux}/bin/mkswap "${path}"
+                  else
+                    echo "${path}: swapfile already exists, skipping creation"
+                  fi
+                '';
+            };
+        })
+      ];
     };
   flake.modules.nixos.swap-partition =
     { lib, config, ... }:
