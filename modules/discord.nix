ca314e55cbc4e596db895ed6a50bee6fdc4c5620
PlumJam <git@plumj.am>
2026-07-14 13:24:28 +0200

    tack: bump inputs (excluding grove)
    Keeping grove pinned until db layer is rewritten for graft.
    
    Had to disable vesktop, see comment.
    
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/discord.nix
+++ modules/discord.nix
@@ -1,12 +1,13 @@
 {
   flake.modules.common.discord =
-    { pkgs, lib, ... }:
-    let
-      inherit (lib.lists) singleton;
-    in
+    # { pkgs, lib, ... }:
+    # let
+    # inherit (lib.lists) singleton;
+    # in
     {
       hjem.extraModule = {
-        packages = singleton pkgs.vesktop;
+        # TODO: waiting for new version (broken by electron): <https://github.com/NixOS/nixpkgs/issues/542512>
+        # packages = singleton pkgs.vesktop;
       };
     };
 }
