79d165c7169596a556731f0402de39e4e3f1e0b1
PlumJam <git@plumj.am>
2026-07-15 19:43:54 +0200

    hjem: add `hjemModule` option for extra modules
    Signed-off-by: PlumJam <git@plumj.am>

--- modules/hjem.nix
+++ modules/hjem.nix
@@ -1,9 +1,8 @@
 { inputs, lib, ... }:
 let
-  inherit (lib.lists) singleton;
+  inherit (lib.lists) optional singleton;
   inherit (lib.options) mkOption;
   inherit (lib.types) deferredModule nullOr;
-  inherit (lib.modules) mkIf;
 
   mkHjemModule =
     hjemModule:
@@ -43,9 +42,19 @@ let
         '';
       };
 
-      config.hjem.extraModules = mkIf (config.hjem.extraModule != null) [
-        config.hjem.extraModule
-      ];
+      options.hjemModule = mkOption {
+        type = nullOr deferredModule;
+        default = null;
+        description = ''
+          Single module to be evaluated as a part of the users module
+          inside `config.hjem.users.<username>`. Use this instead of
+          `extraModules` when you only have one module to add.
+        '';
+      };
+
+      config.hjem.extraModules =
+        optional (config.hjem.extraModule != null) config.hjem.extraModule
+        ++ optional (config.hjemModule != null) config.hjemModule;
     };
 in
 {
